df01d53f8ed25af5f601191cc84cef75834b9f6f
Kaya Tatar <tatark@mit.edu>
2015-12-06 03:10:15 +0100

    diphotonTree and diphotonSkim

--- ForestSkimmers/photons/diphotonSkim.C
+++ ForestSkimmers/photons/diphotonSkim.C
@@ -1,5 +1,5 @@
 /*
- * macro to analyze and save di-Electron spectrum
+ * macro to analyze and save di-Photon spectrum
  */
 
 #include <TFile.h>
@@ -11,17 +11,17 @@
 #include <iomanip>
 
 #include "../../CorrelationTuple/EventMatcher.h"
-#include "../../TreeHeaders/dielectronTree.h"
+#include "../../TreeHeaders/diphotonTree.h"
 #include "../../CutConfigurations/CutConfigurationsParser.h"
 #include "../../TreeHeaders/CutConfigurationTree.h"
 
 const long MAXTREESIZE = 200000000000; // set maximum tree size from 10 GB to 100 GB, so that the code does not switch to a new file after 10 GB7
 
-void dielectronSkim(const char* configFile, const char* inputFile, const char* outputFile = "out_dielectronSkim.root");
+void diphotonSkim(const char* configFile, const char* inputFile, const char* outputFile = "out_diphotonSkim.root");
 
-void dielectronSkim(const char* configFile, const char* inputFile, const char* outputFile)
+void diphotonSkim(const char* configFile, const char* inputFile, const char* outputFile)
 {
-       std::cout<<"running dielectronSkim()"   <<std::endl;
+       std::cout<<"running diphotonSkim()"   <<std::endl;
        std::cout<<"configFile = "<< configFile  <<std::endl;
        std::cout<<"inputFile  = "<< inputFile  <<std::endl;
        std::cout<<"outputFile = "<< outputFile <<std::endl;
@@ -45,25 +45,29 @@ void dielectronSkim(const char* configFile, const char* inputFile, const char* o
 
        CutConfiguration config = CutConfigurationsParser::Parse(configFile);
        TTree* configTree = setupConfigurationTreeForWriting(config);
+       int cut_nPho;
        int cut_nEle;
        if (config.isValid) {
+           cut_nPho = config.proc[CUTS::kSKIM].obj[CUTS::kPHOTON].i[CUTS::PHO::k_nPhotons];
            cut_nEle = config.proc[CUTS::kSKIM].obj[CUTS::kELECTRON].i[CUTS::ELE::k_nEle];
        }
        else {
-           cut_nEle = 2;
+           cut_nPho = 1;
+           cut_nEle = 0;
        }
 
+       std::cout<<"cut_nPho = "<<cut_nPho<<std::endl;
        std::cout<<"cut_nEle = "<<cut_nEle<<std::endl;
 
        // output tree variables
        TTree *outputTreeHLT           = treeHLT->CloneTree(0);
        TTree *outputTreeggHiNtuplizer = ggHiNtuplizerTree->CloneTree(0);
 
-       TTree *diElectronTree = new TTree("dielectron","electron pairs");
-       diElectronTree->SetMaxTreeSize(MAXTREESIZE);
+       TTree *diPhotonTree = new TTree("diphoton","photon pairs that match to an electron");
+       diPhotonTree->SetMaxTreeSize(MAXTREESIZE);
 
-       dielectron diEle;
-       branchDiElectronTree(diElectronTree, diEle);
+       diphoton diPho;
+       branchDiPhotonTree(diPhotonTree, diPho);
 
        EventMatcher* em = new EventMatcher();
        Long64_t duplicateEntries = 0;
@@ -88,15 +92,15 @@ void dielectronSkim(const char* configFile, const char* inputFile, const char* o
                continue;
            }
 
-           // skip if there are no electron to study
-           if(ggHi.nEle < cut_nEle)  continue;
+           // skip if there are no photons or electrons to study
+           if(ggHi.nPho < cut_nPho || ggHi.nEle < cut_nEle)  continue;
            entriesAnalyzed++;
 
-           makeDiElectronPairs(ggHi,diEle);
+           makeDiPhotonPairs(ggHi, diPho);
 
            outputTreeHLT->Fill();
            outputTreeggHiNtuplizer->Fill();
-           diElectronTree->Fill();
+           diPhotonTree->Fill();
        }
        std::cout<< "Loop ENDED : " << eventTreePath.c_str() <<std::endl;
        std::cout << "entries            = " << entries << std::endl;
@@ -104,12 +108,12 @@ void dielectronSkim(const char* configFile, const char* inputFile, const char* o
        std::cout << "entriesAnalyzed    = " << entriesAnalyzed << std::endl;
        std::cout << "outputTreeHLT->GetEntries()           = " << outputTreeHLT->GetEntries() << std::endl;
        std::cout << "outputTreeggHiNtuplizer->GetEntries() = " << outputTreeggHiNtuplizer->GetEntries() << std::endl;
-       std::cout << "diElectronTree->GetEntries()          = " << diElectronTree->GetEntries() << std::endl;
+       std::cout << "diPhotonTree->GetEntries()          = " << diPhotonTree->GetEntries() << std::endl;
 
        // overwrite existing trees
        outputTreeHLT->Write("", TObject::kOverwrite);
        outputTreeggHiNtuplizer->Write("", TObject::kOverwrite);
-       diElectronTree->Write("", TObject::kOverwrite);
+       diPhotonTree->Write("", TObject::kOverwrite);
 
        configTree->Write("", TObject::kOverwrite);
 
@@ -122,18 +126,18 @@ int main(int argc, char** argv)
 {
     if (argc == 4)
     {
-        dielectronSkim(argv[1], argv[2], argv[3]);
+        diphotonSkim(argv[1], argv[2], argv[3]);
         return 0;
     }
     else if (argc == 3)
     {
-        dielectronSkim(argv[1], argv[2]);
+        diphotonSkim(argv[1], argv[2]);
         return 0;
     }
     else
     {
         std::cout << "Usage : \n" <<
-                "./dielectronSkim.exe <configFile> <inputFile> <outputFile>"
+                "./diphotonSkim.exe <configFile> <inputFile> <outputFile>"
                 << std::endl;
         return 1;
     }
