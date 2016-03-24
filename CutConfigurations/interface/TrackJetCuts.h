45331ff2b0710ff989e39afe63e369bf8005570f
Kaya Tatar <tatark@mit.edu>
2016-03-24 09:19:36 +0100

    add TrackJetCuts.h

---
+++ CutConfigurations/interface/TrackJetCuts.h
@@ -0,0 +1,31 @@
+#ifndef CUTCONFIGURATIONS_tjt
+#define CUTCONFIGURATIONS_tjt
+
+namespace CUTS {
+  namespace TJT {
+    enum F_CUTS{
+      k_dR,
+      kN_F_CUTS
+    };
+
+    std::string F_CUTS_LABELS[kN_F_CUTS] = {
+      "dR"
+    };
+
+    enum I_CUTS{
+      kN_I_CUTS
+    };
+
+    std::string I_CUTS_LABELS[kN_I_CUTS] = {
+    };
+
+    enum S_CUTS{
+      kN_S_CUTS
+    };
+
+    std::string S_CUTS_LABELS[kN_S_CUTS] = {};
+
+  };
+};
+
+#endif
