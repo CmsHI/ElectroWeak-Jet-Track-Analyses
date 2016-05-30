30217cbc3622b6ad94fd4136e9aca0869f7d7a6d
R. Alex Barbieri <luck@mit.edu>
2016-05-30 16:33:46 -0400

    run from top level directory so that L2L3 corrections are picked up properly

--- ShellScripts/runAllGammaJet.sh
+++ ShellScripts/runAllGammaJet.sh
@@ -10,42 +10,42 @@ mkdir -p $OUTDIR
 echo "### PbPb Data/MC ###"
 
 echo "## Skimming"
-INFILE=PbPb_Data_HiForest.list
+INFILE=./ShellScripts/PbPb_Data_HiForest.list
 OUTFILE=PbPb_Data_gammaJetSkim.root
-/usr/bin/time ../ForestSkimmers/photons/gammaJetSkim.exe ../CutConfigurations/gammaJet.conf ${INFILE} ${OUTDIR}/${OUTFILE} /export/d00/scratch/luck/GAMMAJETFILES/minBiasJetSkim/minbiasJetSkim.root > ${OUTDIR}/${OUTFILE}.log || echo "ERROR"
+time ./ForestSkimmers/photons/gammaJetSkim.exe ./CutConfigurations/gammaJet.conf ${INFILE} ${OUTDIR}/${OUTFILE} /export/d00/scratch/luck/GAMMAJETFILES/minBiasJetSkim/minbiasJetSkim.root > ${OUTDIR}/${OUTFILE}.log || echo "ERROR"
 
-INFILE=PbPb_MC_HiForest.list
+INFILE=./ShellScripts/PbPb_MC_HiForest.list
 OUTMCSKIM=PbPb_MC_gammaJetSkim.root
-/usr/bin/time ../ForestSkimmers/photons/gammaJetSkim.exe ../CutConfigurations/gammaJet_mc.conf ${INFILE} ${OUTDIR}/${OUTMCSKIM}  /export/d00/scratch/luck/GAMMAJETFILES/PbPb_MC_minbiasJetSkim.root > ${OUTDIR}/${OUTMCSKIM}.log || echo "ERROR"
+time ./ForestSkimmers/photons/gammaJetSkim.exe ./CutConfigurations/gammaJet_mc.conf ${INFILE} ${OUTDIR}/${OUTMCSKIM}  /export/d00/scratch/luck/GAMMAJETFILES/PbPb_MC_minbiasJetSkim.root > ${OUTDIR}/${OUTMCSKIM}.log || echo "ERROR"
 
 echo "## Histogramming"
 INFILE=${OUTDIR}/${OUTFILE}
 INMC=${OUTDIR}/${OUTMCSKIM}
 OUTFILE=PbPb_Data_gammaJetHistogram.root
-/usr/bin/time ../Histogramming/gammaJetHistogram.exe ../CutConfigurations/gammaJet.conf ${INFILE} ${INMC} ${OUTDIR}/${OUTFILE} > ${OUTDIR}/${OUTFILE}.log || echo "ERROR"
+time ./Histogramming/gammaJetHistogram.exe ./CutConfigurations/gammaJet.conf ${INFILE} ${INMC} ${OUTDIR}/${OUTFILE} > ${OUTDIR}/${OUTFILE}.log || echo "ERROR"
 
 INFILE=${OUTDIR}/${OUTMCSKIM}
 INMC=${INFILE}
 OUTFILE=PbPb_MC_gammaJetHistogram.root
-/usr/bin/time ../Histogramming/gammaJetHistogram.exe ../CutConfigurations/gammaJet_mc.conf ${INFILE} ${INMC} ${OUTDIR}/${OUTFILE} > ${OUTDIR}/${OUTFILE}.log || echo "ERROR"
+time ./Histogramming/gammaJetHistogram.exe ./CutConfigurations/gammaJet_mc.conf ${INFILE} ${INMC} ${OUTDIR}/${OUTFILE} > ${OUTDIR}/${OUTFILE}.log || echo "ERROR"
 
 echo "### pp Data/MC ###"
 
 echo "## Skimming"
 OUTFILE=pp_Data_gammaJetSkim.root
-/usr/bin/time ../ForestSkimmers/photons/gammaJetSkim.exe ../CutConfigurations/gammaJet_pp.conf /mnt/hadoop/cms/store/user/luck/2015-Data-promptRECO-photonSkims/pp-photonHLTFilter-v0-HiForest/0.root ${OUTDIR}/${OUTFILE}  > ${OUTDIR}/${OUTFILE}.log || echo "ERROR"
+time ./ForestSkimmers/photons/gammaJetSkim.exe ./CutConfigurations/gammaJet_pp.conf /mnt/hadoop/cms/store/user/luck/2015-Data-promptRECO-photonSkims/pp-photonHLTFilter-v0-HiForest/0.root ${OUTDIR}/${OUTFILE}  > ${OUTDIR}/${OUTFILE}.log || echo "ERROR"
 
-INFILE=pp_MC_HiForest.list
+INFILE=./ShellScripts/pp_MC_HiForest.list
 OUTMCSKIM=pp_MC_gammaJetSkim.root
-/usr/bin/time ../ForestSkimmers/photons/gammaJetSkim.exe ../CutConfigurations/gammaJet_pp_mc.conf $INFILE ${OUTDIR}/${OUTMCSKIM}  > ${OUTDIR}/${OUTMCSKIM}.log || echo "ERROR"
+time ./ForestSkimmers/photons/gammaJetSkim.exe ./CutConfigurations/gammaJet_pp_mc.conf $INFILE ${OUTDIR}/${OUTMCSKIM}  > ${OUTDIR}/${OUTMCSKIM}.log || echo "ERROR"
 
 echo "## Histogramming"
 INFILE=${OUTDIR}/${OUTFILE}
 INMC=${OUTDIR}/${OUTMCSKIM}
 OUTFILE=pp_Data_gammaJetHistogram_smear0_30.root
-/usr/bin/time ../Histogramming/gammaJetHistogram.exe ../CutConfigurations/gammaJet_pp.conf ${INFILE} ${INMC} ${OUTDIR}/${OUTFILE}  > ${OUTDIR}/${OUTFILE}.log || echo "ERROR"
+time ./Histogramming/gammaJetHistogram.exe ./CutConfigurations/gammaJet_pp.conf ${INFILE} ${INMC} ${OUTDIR}/${OUTFILE}  > ${OUTDIR}/${OUTFILE}.log || echo "ERROR"
 
 INFILE=${OUTDIR}/${OUTMCSKIM}
 INMC=${INFILE}
 OUTFILE=pp_MC_gammaJetHistogram_smear0_30.root
-/usr/bin/time ../Histogramming/gammaJetHistogram.exe ../CutConfigurations/gammaJet_pp.conf ${INFILE} ${INMC} ${OUTDIR}/${OUTFILE}  > ${OUTDIR}/${OUTFILE}.log || echo "ERROR"
+time ./Histogramming/gammaJetHistogram.exe ./CutConfigurations/gammaJet_pp.conf ${INFILE} ${INMC} ${OUTDIR}/${OUTFILE}  > ${OUTDIR}/${OUTFILE}.log || echo "ERROR"
