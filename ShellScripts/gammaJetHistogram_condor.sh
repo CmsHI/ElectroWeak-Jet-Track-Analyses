91aea7c3ed1d309d420ea4c477496c7f6961751e
R. Alex Barbieri <luck@mit.edu>
2016-06-15 18:23:04 -0400

    fixes for condor script

--- ShellScripts/gammaJetHistogram_condor.sh
+++ ShellScripts/gammaJetHistogram_condor.sh
@@ -4,27 +4,32 @@ then
   exit 1
 fi
 
-now="submit_$(date +"%Y-%m-%d__%H_%M_%S")"
+now=$(date +"%Y-%m-%d_%H_%M_%S")
 mkdir $now
 echo "Working directory: $now"
 
-mkdir -p $2
+mkdir -p $3
+cp Histogramming/gammaJetHistogram.exe $now/
+mkdir -p $now/CutConfigurations/
+cp $1 $now/CutConfigurations/
+
+realconfig=$(basename $1)
 
 cat > $now/pmerge.condor <<EOF
 Universe     = vanilla
 Initialdir   = $PWD/$now
 Notification = Error
 Executable   = $PWD/$now/merge.sh
-Arguments    = $1 $2 $3 \$(Process) 
+Arguments    = $realconfig $2 $3 \$(Process) 
 GetEnv       = True
-#Output       = \$(Process).out
-#Error        = \$(Process).err
-#Log          = \$(Process).log
+Output       = \$(Process).out
+Error        = \$(Process).err
+Log          = \$(Process).log
 Rank         = Mips
 +AccountingGroup = "group_cmshi.$(whoami)"
 should_transfer_files = YES
 when_to_transfer_output = ON_EXIT
-transfer_input_files = Histogramming/gammaJetHistogram.exe,$1
+transfer_input_files = gammaJetHistogram.exe,$1
 
 Queue $4
 
@@ -32,7 +37,7 @@ EOF
 
 cat > $now/merge.sh <<EOF
 ./gammaJetHistogram.exe \$1 \$2 gammaJetHistogram_\${4}.root $4 \$4
-mv gammaJetHistogram_\${1}.root \$3
+mv gammaJetHistogram_\${4}.root \$3
 EOF
 
 #cat $now/pmerge.condor
