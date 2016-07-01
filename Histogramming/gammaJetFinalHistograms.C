edc193949a47aa63139fbfe7b3122a736ccaef7b
Ran Bi <biran@mit.edu>
2016-07-01 16:21:27 -0400

    add pp mc histograms

--- Histogramming/gammaJetFinalHistograms.C
+++ Histogramming/gammaJetFinalHistograms.C
@@ -9,13 +9,14 @@
 
 int gammaJetFinalHistograms(const TString configFile,
                             const TString PbPb_Data_Filename, const TString PbPb_MC_Filename,
-                            const TString pp_Data_Filename,
+                            const TString pp_Data_Filename, const TString pp_MC_Filename,
                             const TString outputFile) {
     TH1::SetDefaultSumw2();
 
     TFile* PbPb_Data_file = new TFile(PbPb_Data_Filename.Data(), "read");
     TFile* PbPb_MC_file = new TFile(PbPb_MC_Filename.Data(), "read");
     TFile* pp_Data_file = new TFile(pp_Data_Filename.Data(), "read");
+    TFile* pp_MC_file = new TFile(pp_MC_Filename.Data(), "read");
 
     InputConfiguration configInput = InputConfigurationParser::Parse(configFile.Data());
     CutConfiguration configCuts = CutConfigurationParser::Parse(configFile.Data());
@@ -35,18 +36,22 @@ int gammaJetFinalHistograms(const TString configFile,
     TH1D* h_xjg_PbPb_Data[nPtBins][nCentBins];
     TH1D* h_xjg_PbPb_MC[nPtBins][nCentBins];
     TH1D* h_xjg_pp_Data[nPtBins][nCentBins];
+    TH1D* h_xjg_pp_MC[nPtBins][nCentBins];
 
     TH1D* h_dphi_PbPb_Data[nPtBins][nCentBins];
     TH1D* h_dphi_PbPb_MC[nPtBins][nCentBins];
     TH1D* h_dphi_pp_Data[nPtBins][nCentBins];
+    TH1D* h_dphi_pp_MC[nPtBins][nCentBins];
 
     TF1* f_dphi_PbPb_Data[nPtBins][nCentBins];
     TF1* f_dphi_PbPb_MC[nPtBins][nCentBins];
     TF1* f_dphi_pp_Data[nPtBins][nCentBins];
+    TF1* f_dphi_pp_MC[nPtBins][nCentBins];
 
     TH1D* h_ptJet_PbPb_Data[nPtBins][nCentBins];
     TH1D* h_ptJet_PbPb_MC[nPtBins][nCentBins];
     TH1D* h_ptJet_pp_Data[nPtBins][nCentBins];
+    TH1D* h_ptJet_pp_MC[nPtBins][nCentBins];
 
     double iaa_bins[9] = {0, 20, 40, 60, 80, 100, 150, 200, 300};
     TH1D* h_ptJet_PbPb_Data_rebin[nPtBins][nCentBins];
@@ -59,34 +64,42 @@ int gammaJetFinalHistograms(const TString configFile,
             h_xjg_PbPb_Data[i][j] = (TH1D*)PbPb_Data_file->Get(Form("HI/h1D_xjg_ptBin%i_hiBin%i_phoSIG_jetSIG_final_norm", i, j));
             h_xjg_PbPb_MC[i][j] = (TH1D*)PbPb_MC_file->Get(Form("HIMC/h1D_xjg_ptBin%i_hiBin%i_phoSIG_jetSIG_final_norm", i, j));
             h_xjg_pp_Data[i][j] = (TH1D*)pp_Data_file->Get(Form("PP/h1D_xjg_ptBin%i_hiBin%i_phoSIG_jetSIG_final_norm", i, j));
+            h_xjg_pp_MC[i][j] = (TH1D*)pp_MC_file->Get(Form("PPMC/h1D_xjg_ptBin%i_hiBin%i_phoSIG_jetSIG_final_norm", i, j));
 
             h_xjg_PbPb_Data[i][j]->Write(Form("h1D_xjg_ptBin%i_hiBin%i_PbPb_Data", i, j), TObject::kOverwrite);
             h_xjg_PbPb_MC[i][j]->Write(Form("h1D_xjg_ptBin%i_hiBin%i_PbPb_MC", i, j), TObject::kOverwrite);
             h_xjg_pp_Data[i][j]->Write(Form("h1D_xjg_ptBin%i_hiBin%i_pp_Data", i, j), TObject::kOverwrite);
+            h_xjg_pp_MC[i][j]->Write(Form("h1D_xjg_ptBin%i_hiBin%i_pp_MC", i, j), TObject::kOverwrite);
 
             h_dphi_PbPb_Data[i][j] = (TH1D*)PbPb_Data_file->Get(Form("HI/h1D_dphi_ptBin%i_hiBin%i_phoSIG_jetSIG_final_norm", i, j));
             h_dphi_PbPb_MC[i][j] = (TH1D*)PbPb_MC_file->Get(Form("HIMC/h1D_dphi_ptBin%i_hiBin%i_phoSIG_jetSIG_final_norm", i, j));
             h_dphi_pp_Data[i][j] = (TH1D*)pp_Data_file->Get(Form("PP/h1D_dphi_ptBin%i_hiBin%i_phoSIG_jetSIG_final_norm", i, j));
+            h_dphi_pp_MC[i][j] = (TH1D*)pp_MC_file->Get(Form("PPMC/h1D_dphi_ptBin%i_hiBin%i_phoSIG_jetSIG_final_norm", i, j));
 
             h_dphi_PbPb_Data[i][j]->Write(Form("h1D_dphi_ptBin%i_hiBin%i_PbPb_Data", i, j), TObject::kOverwrite);
             h_dphi_PbPb_MC[i][j]->Write(Form("h1D_dphi_ptBin%i_hiBin%i_PbPb_MC", i, j), TObject::kOverwrite);
             h_dphi_pp_Data[i][j]->Write(Form("h1D_dphi_ptBin%i_hiBin%i_pp_Data", i, j), TObject::kOverwrite);
+            h_dphi_pp_MC[i][j]->Write(Form("h1D_dphi_ptBin%i_hiBin%i_pp_MC", i, j), TObject::kOverwrite);
 
             f_dphi_PbPb_Data[i][j] = (TF1*)PbPb_Data_file->Get(Form("HI/fit_dphi_ptBin%i_hiBin%i", i, j));
             f_dphi_PbPb_MC[i][j] = (TF1*)PbPb_MC_file->Get(Form("HIMC/fit_dphi_ptBin%i_hiBin%i", i, j));
             f_dphi_pp_Data[i][j] = (TF1*)pp_Data_file->Get(Form("PP/fit_dphi_ptBin%i_hiBin%i", i, j));
+            f_dphi_pp_MC[i][j] = (TF1*)pp_MC_file->Get(Form("PPMC/fit_dphi_ptBin%i_hiBin%i", i, j));
 
             f_dphi_PbPb_Data[i][j]->Write(Form("fit_dphi_ptBin%i_hiBin%i_PbPb_Data", i, j), TObject::kOverwrite);
             f_dphi_PbPb_MC[i][j]->Write(Form("fit_dphi_ptBin%i_hiBin%i_PbPb_MC", i, j), TObject::kOverwrite);
             f_dphi_pp_Data[i][j]->Write(Form("fit_dphi_ptBin%i_hiBin%i_pp_Data", i, j), TObject::kOverwrite);
+            f_dphi_pp_MC[i][j]->Write(Form("fit_dphi_ptBin%i_hiBin%i_pp_MC", i, j), TObject::kOverwrite);
 
             h_ptJet_PbPb_Data[i][j] = (TH1D*)PbPb_Data_file->Get(Form("HI/h1D_ptJet_ptBin%i_hiBin%i_phoSIG_jetSIG_final_norm", i, j));
             h_ptJet_PbPb_MC[i][j] = (TH1D*)PbPb_MC_file->Get(Form("HIMC/h1D_ptJet_ptBin%i_hiBin%i_phoSIG_jetSIG_final_norm", i, j));
             h_ptJet_pp_Data[i][j] = (TH1D*)pp_Data_file->Get(Form("PP/h1D_ptJet_ptBin%i_hiBin%i_phoSIG_jetSIG_final_norm", i, j));
+            h_ptJet_pp_MC[i][j] = (TH1D*)pp_MC_file->Get(Form("PPMC/h1D_ptJet_ptBin%i_hiBin%i_phoSIG_jetSIG_final_norm", i, j));
 
             h_ptJet_PbPb_Data[i][j]->Write(Form("h1D_ptJet_ptBin%i_hiBin%i_PbPb_Data", i, j), TObject::kOverwrite);
             h_ptJet_PbPb_MC[i][j]->Write(Form("h1D_ptJet_ptBin%i_hiBin%i_PbPb_MC", i, j), TObject::kOverwrite);
             h_ptJet_pp_Data[i][j]->Write(Form("h1D_ptJet_ptBin%i_hiBin%i_pp_Data", i, j), TObject::kOverwrite);
+            h_ptJet_pp_MC[i][j]->Write(Form("h1D_ptJet_ptBin%i_hiBin%i_pp_MC", i, j), TObject::kOverwrite);
 
             h_ptJet_PbPb_Data_rebin[i][j] = (TH1D*)h_ptJet_PbPb_Data[i][j]->Rebin(8, Form("h1D_ptJet_ptBin%i_hiBin%i_PbPb_Data_rebin", i, j), iaa_bins);
             h_ptJet_pp_Data_rebin[i][j] = (TH1D*)h_ptJet_pp_Data[i][j]->Rebin(8, Form("h1D_ptJet_ptBin%i_hiBin%i_pp_Data_rebin", i, j), iaa_bins);
@@ -102,106 +115,131 @@ int gammaJetFinalHistograms(const TString configFile,
     TH1D* h_rjg_centBinAll_PbPb_Data[nPtBins];
     TH1D* h_rjg_centBinAll_PbPb_MC[nPtBins];
     TH1D* h_rjg_centBinAll_pp_Data[nPtBins];
+    TH1D* h_rjg_centBinAll_pp_MC[nPtBins];
 
     TH1D* h_xjg_mean_centBinAll_PbPb_Data[nPtBins];
     TH1D* h_xjg_mean_centBinAll_PbPb_MC[nPtBins];
     TH1D* h_xjg_mean_centBinAll_pp_Data[nPtBins];
+    TH1D* h_xjg_mean_centBinAll_pp_MC[nPtBins];
 
     TH1D* h_dphi_width_cent_PbPb_Data[nPtBins];
     TH1D* h_dphi_width_cent_PbPb_MC[nPtBins];
     TH1D* h_dphi_width_cent_pp_Data[nPtBins];
+    TH1D* h_dphi_width_cent_pp_MC[nPtBins];
 
     TH1D* h_dphi_pedestal_cent_PbPb_Data[nPtBins];
     TH1D* h_dphi_pedestal_cent_PbPb_MC[nPtBins];
     TH1D* h_dphi_pedestal_cent_pp_Data[nPtBins];
+    TH1D* h_dphi_pedestal_cent_pp_MC[nPtBins];
 
     for (int i=0; i<nPtBins; ++i) {
         h_rjg_centBinAll_PbPb_Data[i] = (TH1D*)PbPb_Data_file->Get(Form("HI/h1D_rjg_centBinAll_ptBin%i_phoSIG_jetSIG", i));
         h_rjg_centBinAll_PbPb_MC[i] = (TH1D*)PbPb_MC_file->Get(Form("HIMC/h1D_rjg_centBinAll_ptBin%i_phoSIG_jetSIG", i));
         h_rjg_centBinAll_pp_Data[i] = (TH1D*)pp_Data_file->Get(Form("PP/h1D_rjg_centBinAll_ptBin%i_phoSIG_jetSIG", i));
+        h_rjg_centBinAll_pp_MC[i] = (TH1D*)pp_MC_file->Get(Form("PPMC/h1D_rjg_centBinAll_ptBin%i_phoSIG_jetSIG", i));
 
         h_rjg_centBinAll_PbPb_Data[i]->Write(Form("h1D_rjg_centBinAll_ptBin%i_PbPb_Data", i), TObject::kOverwrite);
         h_rjg_centBinAll_PbPb_MC[i]->Write(Form("h1D_rjg_centBinAll_ptBin%i_PbPb_MC", i), TObject::kOverwrite);
         h_rjg_centBinAll_pp_Data[i]->Write(Form("h1D_rjg_centBinAll_ptBin%i_pp_Data", i), TObject::kOverwrite);
+        h_rjg_centBinAll_pp_MC[i]->Write(Form("h1D_rjg_centBinAll_ptBin%i_pp_MC", i), TObject::kOverwrite);
 
         h_xjg_mean_centBinAll_PbPb_Data[i] = (TH1D*)PbPb_Data_file->Get(Form("HI/h1D_xjg_mean_centBinAll_ptBin%i_phoSIG_jetSIG", i));
         h_xjg_mean_centBinAll_PbPb_MC[i] = (TH1D*)PbPb_MC_file->Get(Form("HIMC/h1D_xjg_mean_centBinAll_ptBin%i_phoSIG_jetSIG", i));
         h_xjg_mean_centBinAll_pp_Data[i] = (TH1D*)pp_Data_file->Get(Form("PP/h1D_xjg_mean_centBinAll_ptBin%i_phoSIG_jetSIG", i));
+        h_xjg_mean_centBinAll_pp_MC[i] = (TH1D*)pp_MC_file->Get(Form("PPMC/h1D_xjg_mean_centBinAll_ptBin%i_phoSIG_jetSIG", i));
 
         h_xjg_mean_centBinAll_PbPb_Data[i]->Write(Form("h1D_xjg_mean_centBinAll_ptBin%i_PbPb_Data", i), TObject::kOverwrite);
         h_xjg_mean_centBinAll_PbPb_MC[i]->Write(Form("h1D_xjg_mean_centBinAll_ptBin%i_PbPb_MC", i), TObject::kOverwrite);
         h_xjg_mean_centBinAll_pp_Data[i]->Write(Form("h1D_xjg_mean_centBinAll_ptBin%i_pp_Data", i), TObject::kOverwrite);
+        h_xjg_mean_centBinAll_pp_MC[i]->Write(Form("h1D_xjg_mean_centBinAll_ptBin%i_pp_MC", i), TObject::kOverwrite);
 
         h_dphi_width_cent_PbPb_Data[i] = (TH1D*)PbPb_Data_file->Get(Form("HI/h1D_dphi_width_centBinAll_ptBin%i", i));
         h_dphi_width_cent_PbPb_MC[i] = (TH1D*)PbPb_MC_file->Get(Form("HIMC/h1D_dphi_width_centBinAll_ptBin%i", i));
         h_dphi_width_cent_pp_Data[i] = (TH1D*)pp_Data_file->Get(Form("PP/h1D_dphi_width_centBinAll_ptBin%i", i));
+        h_dphi_width_cent_pp_MC[i] = (TH1D*)pp_MC_file->Get(Form("PPMC/h1D_dphi_width_centBinAll_ptBin%i", i));
 
         h_dphi_width_cent_PbPb_Data[i]->Write(Form("h1D_dphi_width_centBinAll_ptBin%i_PbPb_Data", i), TObject::kOverwrite);
         h_dphi_width_cent_PbPb_MC[i]->Write(Form("h1D_dphi_width_centBinAll_ptBin%i_PbPb_MC", i), TObject::kOverwrite);
         h_dphi_width_cent_pp_Data[i]->Write(Form("h1D_dphi_width_centBinAll_ptBin%i_pp_Data", i), TObject::kOverwrite);
+        h_dphi_width_cent_pp_MC[i]->Write(Form("h1D_dphi_width_centBinAll_ptBin%i_pp_MC", i), TObject::kOverwrite);
 
         h_dphi_pedestal_cent_PbPb_Data[i] = (TH1D*)PbPb_Data_file->Get(Form("HI/h1D_dphi_pedestal_centBinAll_ptBin%i", i));
         h_dphi_pedestal_cent_PbPb_MC[i] = (TH1D*)PbPb_MC_file->Get(Form("HIMC/h1D_dphi_pedestal_centBinAll_ptBin%i", i));
         h_dphi_pedestal_cent_pp_Data[i] = (TH1D*)pp_Data_file->Get(Form("PP/h1D_dphi_pedestal_centBinAll_ptBin%i", i));
+        h_dphi_pedestal_cent_pp_MC[i] = (TH1D*)pp_MC_file->Get(Form("PPMC/h1D_dphi_pedestal_centBinAll_ptBin%i", i));
 
         h_dphi_pedestal_cent_PbPb_Data[i]->Write(Form("h1D_dphi_pedestal_centBinAll_ptBin%i_PbPb_Data", i), TObject::kOverwrite);
         h_dphi_pedestal_cent_PbPb_MC[i]->Write(Form("h1D_dphi_pedestal_centBinAll_ptBin%i_PbPb_MC", i), TObject::kOverwrite);
         h_dphi_pedestal_cent_pp_Data[i]->Write(Form("h1D_dphi_pedestal_centBinAll_ptBin%i_pp_Data", i), TObject::kOverwrite);
+        h_dphi_pedestal_cent_pp_MC[i]->Write(Form("h1D_dphi_pedestal_centBinAll_ptBin%i_pp_MC", i), TObject::kOverwrite);
     }
 
     TH1D* h_rjg_ptBinAll_PbPb_Data[nPtBins];
     TH1D* h_rjg_ptBinAll_PbPb_MC[nPtBins];
     TH1D* h_rjg_ptBinAll_pp_Data[nPtBins];
+    TH1D* h_rjg_ptBinAll_pp_MC[nPtBins];
 
     TH1D* h_xjg_mean_ptBinAll_PbPb_Data[nPtBins];
     TH1D* h_xjg_mean_ptBinAll_PbPb_MC[nPtBins];
     TH1D* h_xjg_mean_ptBinAll_pp_Data[nPtBins];
+    TH1D* h_xjg_mean_ptBinAll_pp_MC[nPtBins];
 
     TH1D* h_dphi_width_pt_PbPb_Data[nCentBins];
     TH1D* h_dphi_width_pt_PbPb_MC[nCentBins];
     TH1D* h_dphi_width_pt_pp_Data[nCentBins];
+    TH1D* h_dphi_width_pt_pp_MC[nCentBins];
 
     TH1D* h_dphi_pedestal_pt_PbPb_Data[nCentBins];
     TH1D* h_dphi_pedestal_pt_PbPb_MC[nCentBins];
     TH1D* h_dphi_pedestal_pt_pp_Data[nCentBins];
+    TH1D* h_dphi_pedestal_pt_pp_MC[nCentBins];
 
     for (int i=0; i<nCentBins; ++i) {
         h_rjg_ptBinAll_PbPb_Data[i] = (TH1D*)PbPb_Data_file->Get(Form("HI/h1D_rjg_ptBinAll_hiBin%i_phoSIG_jetSIG", i));
         h_rjg_ptBinAll_PbPb_MC[i] = (TH1D*)PbPb_MC_file->Get(Form("HIMC/h1D_rjg_ptBinAll_hiBin%i_phoSIG_jetSIG", i));
         h_rjg_ptBinAll_pp_Data[i] = (TH1D*)pp_Data_file->Get(Form("PP/h1D_rjg_ptBinAll_hiBin%i_phoSIG_jetSIG", i));
+        h_rjg_ptBinAll_pp_MC[i] = (TH1D*)pp_MC_file->Get(Form("PPMC/h1D_rjg_ptBinAll_hiBin%i_phoSIG_jetSIG", i));
 
         h_rjg_ptBinAll_PbPb_Data[i]->Write(Form("h1D_rjg_ptBinAll_hiBin%i_PbPb_Data", i), TObject::kOverwrite);
         h_rjg_ptBinAll_PbPb_MC[i]->Write(Form("h1D_rjg_ptBinAll_hiBin%i_PbPb_MC", i), TObject::kOverwrite);
         h_rjg_ptBinAll_pp_Data[i]->Write(Form("h1D_rjg_ptBinAll_hiBin%i_pp_Data", i), TObject::kOverwrite);
+        h_rjg_ptBinAll_pp_MC[i]->Write(Form("h1D_rjg_ptBinAll_hiBin%i_pp_MC", i), TObject::kOverwrite);
 
         h_xjg_mean_ptBinAll_PbPb_Data[i] = (TH1D*)PbPb_Data_file->Get(Form("HI/h1D_xjg_mean_ptBinAll_hiBin%i_phoSIG_jetSIG", i));
         h_xjg_mean_ptBinAll_PbPb_MC[i] = (TH1D*)PbPb_MC_file->Get(Form("HIMC/h1D_xjg_mean_ptBinAll_hiBin%i_phoSIG_jetSIG", i));
         h_xjg_mean_ptBinAll_pp_Data[i] = (TH1D*)pp_Data_file->Get(Form("PP/h1D_xjg_mean_ptBinAll_hiBin%i_phoSIG_jetSIG", i));
+        h_xjg_mean_ptBinAll_pp_MC[i] = (TH1D*)pp_MC_file->Get(Form("PPMC/h1D_xjg_mean_ptBinAll_hiBin%i_phoSIG_jetSIG", i));
 
         h_xjg_mean_ptBinAll_PbPb_Data[i]->Write(Form("h1D_xjg_mean_ptBinAll_hiBin%i_PbPb_Data", i), TObject::kOverwrite);
         h_xjg_mean_ptBinAll_PbPb_MC[i]->Write(Form("h1D_xjg_mean_ptBinAll_hiBin%i_PbPb_MC", i), TObject::kOverwrite);
         h_xjg_mean_ptBinAll_pp_Data[i]->Write(Form("h1D_xjg_mean_ptBinAll_hiBin%i_pp_Data", i), TObject::kOverwrite);
+        h_xjg_mean_ptBinAll_pp_MC[i]->Write(Form("h1D_xjg_mean_ptBinAll_hiBin%i_pp_MC", i), TObject::kOverwrite);
 
         h_dphi_width_pt_PbPb_Data[i] = (TH1D*)PbPb_Data_file->Get(Form("HI/h1D_dphi_width_ptBinAll_hiBin%i", i));
         h_dphi_width_pt_PbPb_MC[i] = (TH1D*)PbPb_MC_file->Get(Form("HIMC/h1D_dphi_width_ptBinAll_hiBin%i", i));
         h_dphi_width_pt_pp_Data[i] = (TH1D*)pp_Data_file->Get(Form("PP/h1D_dphi_width_ptBinAll_hiBin%i", i));
+        h_dphi_width_pt_pp_MC[i] = (TH1D*)pp_MC_file->Get(Form("PPMC/h1D_dphi_width_ptBinAll_hiBin%i", i));
 
         h_dphi_width_pt_PbPb_Data[i]->Write(Form("h1D_dphi_width_ptBinAll_hiBin%i_PbPb_Data", i), TObject::kOverwrite);
         h_dphi_width_pt_PbPb_MC[i]->Write(Form("h1D_dphi_width_ptBinAll_hiBin%i_PbPb_MC", i), TObject::kOverwrite);
         h_dphi_width_pt_pp_Data[i]->Write(Form("h1D_dphi_width_ptBinAll_hiBin%i_pp_Data", i), TObject::kOverwrite);
+        h_dphi_width_pt_pp_MC[i]->Write(Form("h1D_dphi_width_ptBinAll_hiBin%i_pp_MC", i), TObject::kOverwrite);
 
         h_dphi_pedestal_pt_PbPb_Data[i] = (TH1D*)PbPb_Data_file->Get(Form("HI/h1D_dphi_pedestal_ptBinAll_hiBin%i", i));
         h_dphi_pedestal_pt_PbPb_MC[i] = (TH1D*)PbPb_MC_file->Get(Form("HIMC/h1D_dphi_pedestal_ptBinAll_hiBin%i", i));
         h_dphi_pedestal_pt_pp_Data[i] = (TH1D*)pp_Data_file->Get(Form("PP/h1D_dphi_pedestal_ptBinAll_hiBin%i", i));
+        h_dphi_pedestal_pt_pp_MC[i] = (TH1D*)pp_MC_file->Get(Form("PPMC/h1D_dphi_pedestal_ptBinAll_hiBin%i", i));
 
         h_dphi_pedestal_pt_PbPb_Data[i]->Write(Form("h1D_dphi_pedestal_ptBinAll_hiBin%i_PbPb_Data", i), TObject::kOverwrite);
         h_dphi_pedestal_pt_PbPb_MC[i]->Write(Form("h1D_dphi_pedestal_ptBinAll_hiBin%i_PbPb_MC", i), TObject::kOverwrite);
         h_dphi_pedestal_pt_pp_Data[i]->Write(Form("h1D_dphi_pedestal_ptBinAll_hiBin%i_pp_Data", i), TObject::kOverwrite);
+        h_dphi_pedestal_pt_pp_MC[i]->Write(Form("h1D_dphi_pedestal_ptBinAll_hiBin%i_pp_MC", i), TObject::kOverwrite);
     }
 
     PbPb_Data_file->Close();
     PbPb_MC_file->Close();
     pp_Data_file->Close();
+    pp_MC_file->Close();
 
     output_file->Write("", TObject::kOverwrite);
     output_file->Close();
@@ -210,10 +248,10 @@ int gammaJetFinalHistograms(const TString configFile,
 }
 
 int main(int argc, char* argv[]) {
-    if (argc == 6)
-        return gammaJetFinalHistograms(argv[1], argv[2], argv[3], argv[4], argv[5]);
+    if (argc == 7)
+        return gammaJetFinalHistograms(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6]);
     else
-        printf("Usage: ./gammaJetFinalHistograms.exe <configFile> <PbPb_Data_file> <PbPb_MC_file> <pp_Data_file> <outputFile>\n");
+        printf("Usage: ./gammaJetFinalHistograms.exe <configFile> <PbPb_Data_file> <PbPb_MC_file> <pp_Data_file> <pp_MC_file> <outputFile>\n");
 
     return 1;
 }
