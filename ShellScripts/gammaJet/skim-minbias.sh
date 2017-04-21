514fffced2695155ba9b1bfccea16c9df8e8e0a8
Ran Bi <biran@mit.edu>
2017-04-21 17:27:58 -0400

    update MB HiForest file (new tune)

--- ShellScripts/gammaJet/skim-minbias.sh
+++ ShellScripts/gammaJet/skim-minbias.sh
@@ -18,7 +18,8 @@ INFILE=./ShellScripts/gammaJet/PbPb_Data_MB_HiForest.list
 OUTFILE=PbPb_Data_minbiasJetSkim.root
 ./ForestSkimmers/jets/minBiasJetSkim.exe ./CutConfigurations/gammaJet.conf $INFILE ${OUTDIR}/$OUTFILE > ${OUTDIR}/PbPb_Data_minbiasJetSkim.log &
 
-INFILE=/mnt/hadoop/cms/store/user/rbi/merged/Hydjet_Quenched_MinBias_5020GeV_750-HINPbPbWinter16DR-NoPU_75X_mcRun2_HeavyIon_forest_v2/0.root
+#INFILE=/mnt/hadoop/cms/store/user/rbi/merged/Hydjet_Quenched_MinBias_5020GeV_750-HINPbPbWinter16DR-NoPU_75X_mcRun2_HeavyIon_forest_v2/0.root
+INFILE=/mnt/hadoop/cms/store/user/biran/photon-jet-track/PbPb-MB-Hydjet-Cymbal-170331.root
 OUTFILE=PbPb_MC_minbiasJetSkim.root
 ./ForestSkimmers/jets/minBiasJetSkim.exe ./CutConfigurations/gammaJet_mc.conf $INFILE ${OUTDIR}/$OUTFILE > ${OUTDIR}/PbPb_MC_minbiasJetSkim.log &
 wait
