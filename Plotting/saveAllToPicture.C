0b65010a5074ffce9f38731cda242d7f8251490f
Kaya Tatar <tatark@mit.edu>
2017-05-17 16:54:21 -0400

    saveAllToPicture.C works with wildcards now. If wanted only those histograms/canvases/graphics will be saved whose path match the one of the given wildcards. Example usage : ./saveAllToPicture.exe (--format=<graphicsFormat>) (--wc=<wildcards>) <inputFile> <outputDirectory> also employs ArgumentParser class now.

--- Plotting/saveAllToPicture.C
+++ Plotting/saveAllToPicture.C
@@ -14,45 +14,80 @@
 #include <string>
 #include <iostream>
 
+#include "../Utilities/interface/ArgumentParser.h"
 #include "../Utilities/fileUtil.h"
+#include "../Utilities/systemUtil.h"
 
-void saveAllToPicture(std::string fileName, std::string pictureType = "png", std::string directory = "");
+std::string graphicsFormat;
+std::vector<std::string> wildCards;
 
-void saveAllToPicture(std::string fileName, std::string pictureType, std::string directory)
+void saveAllToPicture(std::string fileName, std::string directory = "");
+
+void saveAllToPicture(std::string fileName, std::string directory)
 {
-   TFile* file = new TFile(fileName.c_str(),"READ");
-   std::cout<< "begin : saving graphics" <<std::endl;
+    std::cout<<"running saveAllToPicture()"<<std::endl;
+    TFile* file = new TFile(fileName.c_str(),"READ");
+    std::cout<< "begin : saving graphics" <<std::endl;
 
-   std::string directoryToBeSavedIn;
-   if(directory == "")   { // no input directory was given.
+    std::string directoryToBeSavedIn;
+    if(directory == "")   { // no input directory was given.
 
-       TString dirName(file->GetName());
-       dirName.ReplaceAll(".root","");
-       gSystem->mkdir(dirName,true);
-       directoryToBeSavedIn=dirName.Data();
-   }
-   else {
-       directoryToBeSavedIn = directory.c_str();
-   }
+        TString dirName(file->GetName());
+        dirName.ReplaceAll(".root","");
+        gSystem->mkdir(dirName,true);
+        directoryToBeSavedIn=dirName.Data();
+    }
+    else {
+        directoryToBeSavedIn = directory.c_str();
+    }
 
-   saveAllHistogramsToPicture (file, pictureType.c_str(), directoryToBeSavedIn);
-   saveAllGraphsToPicture     (file, pictureType.c_str(), directoryToBeSavedIn);
-   saveAllCanvasesToPicture   (file, pictureType.c_str(), directoryToBeSavedIn);
+    // print the input of different options
+    std::cout << "graphicsFormat = " << graphicsFormat.c_str() << std::endl;
+    int nWildCards = wildCards.size();
+    if (nWildCards > 0 && wildCards.at(0).size() > 0) {
+        std::cout << "nWildCards = " << nWildCards << std::endl;
+        for (int i = 0; i<nWildCards; ++i){
+            std::cout << Form("wc[%d] = %s", i, wildCards.at(i).c_str()) << std::endl;
+        }
+    }
 
-   std::cout<< "end : saving graphics" <<std::endl;
+    // save to picture
+    for (std::vector<std::string>::const_iterator it = wildCards.begin() ; it != wildCards.end(); ++it){
 
-   file->Close();
-   std::cout<< "input file has been closed" <<std::endl;
+        std::string regexStr = "";
+        if((*it).size() > 0) regexStr = wildCard2Regex((*it));
+
+        saveHistogramsToPicture (file, regexStr, graphicsFormat, directoryToBeSavedIn);
+        saveGraphsToPicture     (file, regexStr, graphicsFormat, directoryToBeSavedIn);
+        saveCanvasesToPicture   (file, regexStr, graphicsFormat, directoryToBeSavedIn);
+    }
+    std::cout<< "end : saving graphics" <<std::endl;
+
+    std::cout<<"Closing the input file."<<std::endl;
+    file->Close();
+    std::cout<<"running saveAllToPicture() - END"<<std::endl;
 }
 
 int main(int argc, char** argv)
 {
-    if (argc == 4)    saveAllToPicture(argv[1], argv[2], argv[3]);
-    else if (argc == 3)    saveAllToPicture(argv[1], argv[2]);
-    else if (argc == 2)    saveAllToPicture(argv[1]);
+    std::vector<std::string> argStr = ArgumentParser::ParseParameters(argc, argv);
+    int nArgStr = argStr.size();
+
+    std::vector<std::string> argOptions = ArgumentParser::ParseOptions(argc, argv);
+    // parse the input of different options
+    graphicsFormat = (ArgumentParser::ParseOptionInput(ARGUMENTPARSER::format, argOptions).size() > 0 ?
+                      ArgumentParser::ParseOptionInput(ARGUMENTPARSER::format, argOptions).at(0) : "");
+    wildCards = ArgumentParser::ParseOptionInput(ARGUMENTPARSER::wildCard, argOptions);
+
+    // set default values
+    if (graphicsFormat.size() == 0)  graphicsFormat = "png";
+    if (wildCards.size() == 0)  wildCards = {""};
+
+    if (nArgStr == 3)    saveAllToPicture(argStr.at(1), argStr.at(2));
+    else if (nArgStr == 2)    saveAllToPicture(argStr.at(1));
     else {
         std::cout << "Usage : \n" <<
-                "./saveAllToPicture.exe <inputFile> <pictureType> <outputDirectory>"
+                "./saveAllToPicture.exe (--format=<graphicsFormat>) (--wc=<wildcards>) <inputFile> <outputDirectory>"
                 << std::endl;
         return 1;
     }
