ef986f2226562b6bcbb95baeb2faec1cdb887fba
Ran Bi <biran@mit.edu>
2017-05-25 22:33:52 -0400

    remove hard-coded paths to my hadoop and scratch directories from scripts

--- ShellScripts/gammaJet/run-skims.sh
+++ ShellScripts/gammaJet/run-skims.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-if [ $# -lt 4 ]; then
-    echo -e "Usage: ./ShellScipts/gammaJet/run-gamma-jet.sh [0/1] [0/1] [label] [mixing dir]"
-    echo -e "[0: skim + histogram, 1: merge + produce final results]"
-    echo -e "[0: results, 1: systematics]"
+if [ $# -ne 5 ]; then
+    echo -e "Usage: ./ShellScipts/gammaJet/run-skims.sh [0/1] [0/1] [hadoop dir] [output dir] [mixing dir]"
+    echo -e "arg 1: [0: skim + histogram, 1: merge + produce final results]"
+    echo -e "arg 2: [0: results, 1: systematics]"
+    echo -e "[hadoop dir] contains condor output, [output dir] will contain merged skims/histograms"
     exit 1
 fi
 
 set -x
 
-LABEL=$3
-
-OUTDIR=/export/d00/scratch/biran/gamma-jet/${LABEL}
+HADOOPDIR=$3
+mkdir -p $HADOOPDIR
+OUTDIR=$4
 mkdir -p $OUTDIR
-H_INDIR=/mnt/hadoop/cms/store/user/biran/gamma-jet/${LABEL}/
-mkdir -p $H_INDIR
 
 SAMPLE=(PbPb_Data PbPb_MC pp_Data pp_MC)
 INFILE=(./ShellScripts/gammaJet/PbPb_Data_HiForest.list ./ShellScripts/gammaJet/PbPb_MC_Flt30_HiForest.list /mnt/hadoop/cms/store/user/luck/2015-Data-promptRECO-photonSkims/pp-photonHLTFilter-v0-HiForest/0.root ./ShellScripts/gammaJet/pp_MC_HiForest.list)
@@ -36,8 +35,8 @@ if [ $1 -eq 0 ]; then
         for ITERSYS in $(seq 0 $NSYS)
         do
             CONFSUFFIX=${CONFSUFFIXES[ITERSAM]}${SYSTEMATIC[ITERSYS]}
-            mkdir -p $H_INDIR/${SAMPLE[ITERSAM]}${SYSTEMATIC[ITERSYS]}_unmerged/
-            ./ShellScripts/gammaJet/gamma-jet-condor.sh ./CutConfigurations/gammaJet${CONFSUFFIX}.conf ${INFILE[ITERSAM]} $H_INDIR/${SAMPLE[ITERSAM]}${SYSTEMATIC[ITERSYS]}_unmerged/ ${NJOBS[ITERSAM]} ${MIXINGSAMPLE[ITERSAM]}
+            mkdir -p $HADOOPDIR/${SAMPLE[ITERSAM]}${SYSTEMATIC[ITERSYS]}_unmerged/
+            ./ShellScripts/gammaJet/gamma-jet-condor.sh ./CutConfigurations/gammaJet${CONFSUFFIX}.conf ${INFILE[ITERSAM]} $HADOOPDIR/${SAMPLE[ITERSAM]}${SYSTEMATIC[ITERSYS]}_unmerged/ ${NJOBS[ITERSAM]} ${MIXINGSAMPLE[ITERSAM]}
         done
     done
 fi
@@ -45,17 +44,18 @@ fi
 if [ $1 -eq 1 ]; then
     for ITERSAM in 0 1 2 3
     do
-        hadd $OUTDIR/${SAMPLE[ITERSAM]}_gammaJetSkim.root $H_INDIR/${SAMPLE[ITERSAM]}_unmerged/gammaJetSkim*.root
+        hadd $OUTDIR/${SAMPLE[ITERSAM]}_gammaJetSkim.root $HADOOPDIR/${SAMPLE[ITERSAM]}_unmerged/gammaJetSkim*.root
     done
 
     for ITERSYS in $(seq 0 $NSYS)
     do
         for ITERSAM in 0 1 2 3
         do
-            hadd $OUTDIR/${SAMPLE[ITERSAM]}_gammaJetHistogram${SYSTEMATIC[ITERSYS]}.root $H_INDIR/${SAMPLE[ITERSAM]}${SYSTEMATIC[ITERSYS]}_unmerged/gammaJetHistogram*.root
+            hadd $OUTDIR/${SAMPLE[ITERSAM]}_gammaJetHistogram${SYSTEMATIC[ITERSYS]}.root $HADOOPDIR/${SAMPLE[ITERSAM]}${SYSTEMATIC[ITERSYS]}_unmerged/gammaJetHistogram*.root
             CONFSUFFIX=${CONFSUFFIXES[ITERSAM]}${SYSTEMATIC[ITERSYS]}
             ./Histogramming/gammaJetHistogramArithmetic.exe ./CutConfigurations/gammaJet${CONFSUFFIX}.conf $OUTDIR/${SAMPLE[ITERSAM]}_gammaJetHistogram${SYSTEMATIC[ITERSYS]}.root $OUTDIR/${SAMPLE[ITERSAM]}_gammaJetHistogramArithmetic${SYSTEMATIC[ITERSYS]}.root
         done
+
         ./Histogramming/gammaJetFinalHistograms.exe ./CutConfigurations/gammaJet${SYSTEMATIC[ITERSYS]}.conf ${OUTDIR}/PbPb_Data_gammaJetHistogramArithmetic${SYSTEMATIC[ITERSYS]}.root ${OUTDIR}/PbPb_MC_gammaJetHistogramArithmetic${SYSTEMATIC[ITERSYS]}.root ${OUTDIR}/pp_Data_gammaJetHistogramArithmetic${SYSTEMATIC[ITERSYS]}.root ${OUTDIR}/pp_MC_gammaJetHistogramArithmetic${SYSTEMATIC[ITERSYS]}.root ${OUTDIR}/gammaJetHistograms${SYSTEMATIC[ITERSYS]}.root
     done
 fi
--- ShellScripts/gammaJet/run-systematics.sh
+++ ShellScripts/gammaJet/run-systematics.sh
@@ -1,42 +1,41 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo -e "Usage: ./ShellScipts/gammaJet/run-systematics.sh [label]"
+if [ $# -ne 2 ]; then
+    echo -e "Usage: ./ShellScipts/gammaJet/run-systematics.sh [input dir] [output dir]"
+    echo -e "[input dir] should contain *_gammaJetSkim.root files"
     exit 1
 fi
 
 set -x
 
-LABEL=$1
-
-OUTDIR=/export/d00/scratch/biran/gamma-jet/${LABEL}
+INDIR=$1
+OUTDIR=$2
 mkdir -p $OUTDIR
 
 SAMPLE=(PbPb_Data PbPb_MC pp_Data pp_MC)
 CONFSUFFIXES=("" _mc _pp _pp_mc)
 
 SYSTEMATIC=(_JER _PES _PURITY_UP _PURITY_DOWN _ELE_REJ)
-
 for ITERSYS in 0 1 2 3 4
 do
     for ITERSAM in 0 1 2 3
     do
         CONFSUFFIX=${CONFSUFFIXES[ITERSAM]}${SYSTEMATIC[ITERSYS]}
-        ./Histogramming/gammaJetHistogram.exe ./CutConfigurations/gammaJet${CONFSUFFIX}.conf $OUTDIR/${SAMPLE[ITERSAM]}_gammaJetSkim.root $OUTDIR/${SAMPLE[ITERSAM]}_gammaJetHistogram${SYSTEMATIC[ITERSYS]}.root
+        ./Histogramming/gammaJetHistogram.exe ./CutConfigurations/gammaJet${CONFSUFFIX}.conf $INDIR/${SAMPLE[ITERSAM]}_gammaJetSkim.root $OUTDIR/${SAMPLE[ITERSAM]}_gammaJetHistogram${SYSTEMATIC[ITERSYS]}.root
         ./Histogramming/gammaJetHistogramArithmetic.exe ./CutConfigurations/gammaJet${CONFSUFFIX}.conf $OUTDIR/${SAMPLE[ITERSAM]}_gammaJetHistogram${SYSTEMATIC[ITERSYS]}.root $OUTDIR/${SAMPLE[ITERSAM]}_gammaJetHistogramArithmetic${SYSTEMATIC[ITERSYS]}.root
     done
 
     ./Histogramming/gammaJetFinalHistograms.exe ./CutConfigurations/gammaJet${SYSTEMATIC[ITERSYS]}.conf ${OUTDIR}/PbPb_Data_gammaJetHistogramArithmetic${SYSTEMATIC[ITERSYS]}.root ${OUTDIR}/PbPb_MC_gammaJetHistogramArithmetic${SYSTEMATIC[ITERSYS]}.root ${OUTDIR}/pp_Data_gammaJetHistogramArithmetic${SYSTEMATIC[ITERSYS]}.root ${OUTDIR}/pp_MC_gammaJetHistogramArithmetic${SYSTEMATIC[ITERSYS]}.root ${OUTDIR}/gammaJetHistograms${SYSTEMATIC[ITERSYS]}.root
 done
 
-ITERSYS=5
+# photon isolation systematics
 for ITERSAM in 1 3
 do
-    CONFSUFFIX=${CONFSUFFIXES[ITERSAM]}${SYSTEMATIC[ITERSYS]}
-    ./Histogramming/gammaJetHistogram.exe ./CutConfigurations/gammaJet${CONFSUFFIX}.conf $OUTDIR/${SAMPLE[ITERSAM]}_gammaJetSkim.root $OUTDIR/${SAMPLE[ITERSAM]}_gammaJetHistogram${SYSTEMATIC[ITERSYS]}.root
-    ./Histogramming/gammaJetHistogramArithmetic.exe ./CutConfigurations/gammaJet${CONFSUFFIX}.conf $OUTDIR/${SAMPLE[ITERSAM]}_gammaJetHistogram${SYSTEMATIC[ITERSYS]}.root $OUTDIR/${SAMPLE[ITERSAM]}_gammaJetHistogramArithmetic${SYSTEMATIC[ITERSYS]}.root
+    CONFSUFFIX=${CONFSUFFIXES[ITERSAM]}_ISO
+    ./Histogramming/gammaJetHistogram.exe ./CutConfigurations/gammaJet${CONFSUFFIX}.conf $INDIR/${SAMPLE[ITERSAM]}_gammaJetSkim.root $OUTDIR/${SAMPLE[ITERSAM]}_gammaJetHistogram_ISO.root
+    ./Histogramming/gammaJetHistogramArithmetic.exe ./CutConfigurations/gammaJet${CONFSUFFIX}.conf $OUTDIR/${SAMPLE[ITERSAM]}_gammaJetHistogram_ISO.root $OUTDIR/${SAMPLE[ITERSAM]}_gammaJetHistogramArithmetic_ISO.root
 done
 
-./Histogramming/gammaJetFinalHistograms.exe ./CutConfigurations/gammaJet.conf ${OUTDIR}/PbPb_Data_gammaJetHistogramArithmetic.root ${OUTDIR}/PbPb_MC_gammaJetHistogramArithmetic${SYSTEMATIC[ITERSYS]}.root ${OUTDIR}/pp_Data_gammaJetHistogramArithmetic.root ${OUTDIR}/pp_MC_gammaJetHistogramArithmetic${SYSTEMATIC[ITERSYS]}.root ${OUTDIR}/gammaJetHistograms${SYSTEMATIC[ITERSYS]}.root
+./Histogramming/gammaJetFinalHistograms.exe ./CutConfigurations/gammaJet.conf ${OUTDIR}/PbPb_Data_gammaJetHistogramArithmetic.root ${OUTDIR}/PbPb_MC_gammaJetHistogramArithmetic_ISO.root ${OUTDIR}/pp_Data_gammaJetHistogramArithmetic.root ${OUTDIR}/pp_MC_gammaJetHistogramArithmetic_ISO.root ${OUTDIR}/gammaJetHistograms_ISO.root
 
 # calculate systematics
