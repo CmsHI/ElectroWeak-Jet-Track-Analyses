28a98214f2f7ef1299995e03108a88a29132bb92
Ran Bi <biran@mit.edu>
2017-06-14 10:37:27 -0400

    update gammajet script

--- ShellScripts/gammaJet/run-all.sh
+++ ShellScripts/gammaJet/run-all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ $# -ne 3 ]]; then
-    echo -e "Usage: ./ShellScripts/gammaJet/run-all.sh [input dir] [output dir] [0/1/2]"
-    echo -e "arg 3: [0: histogram, 1: arithmetic, 2: final]"
+    echo -e "Usage: ./ShellScripts/gammaJet/run-all.sh [input dir] [output dir] [0/1/2/3]"
+    echo -e "arg 3: [0: histogram, 1: arithmetic, 2: final, 3: systematics]"
     echo -e "[input dir] should contain input files"
     echo -e "[output dir] will contain output files up to final output"
     exit 1
@@ -12,12 +12,14 @@ INDIR=$1
 OUTDIR=$2
 mkdir -p $OUTDIR
 
-./ShellScripts/gammaJet/run-nominal.sh $@
+if [[ $3 -ne 3 ]]; then
+    ./ShellScripts/gammaJet/run-nominal.sh $@
 
-ISODIR=$([[ $3 -eq 2 ]] && echo "$INDIR" || echo "$OUTDIR")
-cp ${ISODIR}/*_Data_gammaJetHistogramArithmetic.root ${OUTDIR}/
+    ISODIR=$([[ $3 -eq 2 ]] && echo "$INDIR" || echo "$OUTDIR")
+    cp ${ISODIR}/*_Data_gammaJetHistogramArithmetic.root ${OUTDIR}/
 
-./ShellScripts/gammaJet/run-systematics.sh $@ 1
+    ./ShellScripts/gammaJet/run-systematics.sh $@
+fi
 
 # write systematics config file
 SYSLIST=${OUTDIR}/gammaJetSystematics.list
