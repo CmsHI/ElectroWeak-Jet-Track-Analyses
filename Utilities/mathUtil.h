8280e76a528d42fa117d990bea5727494f7d1fd6
Kaya Tatar <tatark@mit.edu>
2017-06-16 15:45:36 -0400

    utilities related to math

---
+++ Utilities/mathUtil.h
@@ -0,0 +1,49 @@
+/*
+ * utilities related to math.
+ */
+
+#include <TString.h>
+#include <TMath.h>
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+#ifndef MATHUTIL_H_
+#define MATHUTIL_H_
+
+double fnc_DSCB(double* xx, double* params);
+
+/*
+ * Double sided crystal ball function
+ * The default crystal ball function has its tail to the left of Gaussian core.
+ * For details of the crystal function see : https://en.wikipedia.org/wiki/Crystal_Ball_function
+ */
+double fnc_DSCB(double* xx, double* params)
+{
+    double x   = xx[0];
+    // gaussian core
+    double N     = params[0];
+    double mean  = params[1];
+    double sigma = params[2];
+    // transition parameters
+    double a1  = params[3];
+    double p1  = params[4];
+    double a2  = params[5];
+    double p2  = params[6];
+
+    double u   = (x-mean)/sigma;
+    double A1  = TMath::Power(p1/TMath::Abs(a1),p1)*TMath::Exp(-a1*a1/2);
+    double A2  = TMath::Power(p2/TMath::Abs(a2),p2)*TMath::Exp(-a2*a2/2);
+    double B1  = p1/TMath::Abs(a1) - TMath::Abs(a1);
+    double B2  = p2/TMath::Abs(a2) - TMath::Abs(a2);
+
+    double res = N;
+    if      (u < -a1) res *= A1*TMath::Power(B1-u,-p1);
+    else if (u < a2)  res *= TMath::Exp(-u*u/2);
+    else              res *= A2*TMath::Power(B2+u,-p2);
+    return res;
+}
+
+#endif /* MATHUTIL_H_ */
+
