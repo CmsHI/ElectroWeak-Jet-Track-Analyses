10a2be693eddd39be1bbc32fd1dc7406a05ccb4c
Ran Bi <biran@mit.edu>
2017-06-21 17:08:43 -0400

    modify script to run on final histograms file

--- run-one-sigma.sh
+++ run-one-sigma.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-HIST_LIST=histograms.list
-./emit-histogram-names.sh $HIST_LIST
+if [[ $# -ne 3 ]]; then
+    echo "Usage: ./run-one-sigma.sh [nominal file] [input dir] [output dir]"
+    exit 1;
+fi
 
 SYSTYPE=(JER JES_UP JES_DOWN)
 
 for ISYS in 0 1 2; do
-    VAR_FILE_LIST=variations_${SYSTYPE[ISYS]}.list
-    ls /export/d00/scratch/biran/gamma-jet/2017-06-20-incremental/PbPb_Data_${SYSTYPE[ISYS]}/gammaJetHistogramArithmetic_*.root > $VAR_FILE_LIST
+    OUTPUTDIR=$3/${SYSTYPE[ISYS]}
 
-    ./one_sigma_systematics /export/d00/scratch/biran/gamma-jet/2017-06-20-incremental/PbPb_Data_gammaJetHistogramArithmetic.root $VAR_FILE_LIST $HIST_LIST ${SYSTYPE[ISYS]}
+    HIST_LIST=$OUTPUTDIR/histogram-names.list
+    ./emit-histogram-names.sh $HIST_LIST
+
+    VAR_FILE_LIST=variations_${SYSTYPE[ISYS]}.list
+    ls $2/PbPb_Data_${SYSTYPE[ISYS]}/gammaJetHistograms_*.root > $VAR_FILE_LIST
+
+    ./one_sigma_systematics $1 $VAR_FILE_LIST $HIST_LIST ${SYSTYPE[ISYS]}
 done
