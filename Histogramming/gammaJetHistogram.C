0ee33d1272a945dd06df61a5891d0f51479e2f32
Ran Bi <biran@mit.edu>
2017-06-21 22:12:31 -0400

    update jet smearing parameters

--- Histogramming/gammaJetHistogram.C
+++ Histogramming/gammaJetHistogram.C
@@ -288,24 +288,24 @@ int gammaJetHistogram(const TString configFile, const TString inputFile, const T
     jetCorrector resolutionJetSmear[6];
 
     // smear 0-10 %
-    std::vector<double> CSN_HI_cent0010 = {0.06, 1.23, 8.38};
+    std::vector<double> CSN_HI_cent0010 = {0.06, 1.241, 8.421};
     std::vector<double> CSN_phi_HI_cent0010 = {-3.18781 / 10000000, 0.125911, 2.23898};
     // smear 10-30 %
-    std::vector<double> CSN_HI_cent1030 = {0.06, 1.23, 5.88};
+    std::vector<double> CSN_HI_cent1030 = {0.06, 1.241, 5.537};
     std::vector<double> CSN_phi_HI_cent1030 = {1.14344 / 100000, 0.179847, 1.56128};
     // smear 30-50 %
-    std::vector<double> CSN_HI_cent3050 = {0.06, 1.23, 3.24};
+    std::vector<double> CSN_HI_cent3050 = {0.059, 1.239, 2.372};
     std::vector<double> CSN_phi_HI_cent3050 = {0.0145775, 0.1222, 1.21751};
     // smear 50-100 %
-    std::vector<double> CSN_HI_cent50100 = {0.06, 1.23, 0};
+    std::vector<double> CSN_HI_cent50100 = {0.059, 1.239, 0};
     std::vector<double> CSN_phi_HI_cent50100 = {-0.0073078, 0.168879, 0.798885};
 
     // 0-30 and 30-100 are necessary only for smeared pp JER computation
     // smear 0-30 %
-    std::vector<double> CSN_HI_cent0030 = {0.06, 1.23, 7.38};
+    std::vector<double> CSN_HI_cent0030 = {0.059, 1.239, 0};
     std::vector<double> CSN_phi_HI_cent0030 = {-1.303 / 1000000, 0.1651, 1.864};
     // smear 30-100 %
-    std::vector<double> CSN_HI_cent30100 = {0.06, 1.23, 2.1};
+    std::vector<double> CSN_HI_cent30100 = {0.06, 1.241, 6.827};
     std::vector<double> CSN_phi_HI_cent30100 = {-2.013 / 100000000, 0.1646, 1.04};
 
 
