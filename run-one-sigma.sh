fb11b79e5bc34a7436329b5466d93f86c9cbf29f
Ran Bi <biran@mit.edu>
2017-06-21 21:11:12 -0400

    move created list

--- run-one-sigma.sh
+++ run-one-sigma.sh
@@ -13,11 +13,12 @@ SYSTYPE=(JER JES_UP JES_DOWN)
 
 for ISYS in 0 1 2; do
     OUTPUT_FINAL=$OUTPUT/${SYSTYPE[ISYS]}
+    mkdir -p $OUTPUT_FINAL
 
     HIST_LIST=$OUTPUT_FINAL/histogram-names.list
     ./emit-histogram-names.sh $HIST_LIST
 
-    VAR_FILE_LIST=variations_${SYSTYPE[ISYS]}.list
+    VAR_FILE_LIST=$OUTPUT_FINAL/variations_${SYSTYPE[ISYS]}.list
     ls $INPUT/PbPb_Data_${SYSTYPE[ISYS]}/gammaJetHistograms_*.root > $VAR_FILE_LIST
 
     ./one_sigma_systematics $NOMINAL $VAR_FILE_LIST $HIST_LIST $OUTPUT_FINAL/gammaJetHistograms_${SYSTYPE[ISYS]}
