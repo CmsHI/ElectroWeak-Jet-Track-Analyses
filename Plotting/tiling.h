52269e7e6519a024363f185bb6eb0bb85b76dbce
Ran Bi <biran@mit.edu>
2017-07-06 22:52:01 -0400

    fully featured tiling class

--- Plotting/tiling.h
+++ Plotting/tiling.h
@@ -96,14 +96,8 @@ class tiling {
                                   int font, float font_size, int align,
                                   int col, int row);
 
-    void set_sizes(TH1* h1, int font,
-                   float x_title_size, float y_title_size,
-                   float x_label_size, float y_label_size,
-                   float x_tick_size, float y_tick_size,
-                   float x_title_offset, float y_title_offset,
-                   float x_label_offset, float y_label_offset,
-                   int col, int row);
-    void set_sizes(TGraph* g1, int font,
+    template <class T>
+    void set_sizes(T* h1, int font,
                    float x_title_size, float y_title_size,
                    float x_label_size, float y_label_size,
                    float x_tick_size, float y_tick_size,
@@ -122,10 +116,10 @@ class tiling {
     float canvas_margin_left, canvas_margin_right;
     float canvas_margin_top, canvas_margin_bottom;
 
-    float canvas_frame_ratio_horiz, canvas_frame_ratio_vert;
+    float cfratio_horiz, cfratio_vert;
 
     std::vector<float> tile_edges_horiz, tile_edges_vert;
-    std::vector<float> frame_edges_horiz, frame_edges_vert;
+    std::vector<float> fedges_horiz, fedges_vert;
     std::vector<float> tile_widths, tile_heights;
 
     void tile();
@@ -149,50 +143,50 @@ class tiling {
 };
 
 void tiling::tile() {
-    canvas_frame_ratio_horiz = columns + lmargin + rmargin;
-    canvas_margin_left = lmargin / canvas_frame_ratio_horiz;
-    canvas_margin_right = rmargin / canvas_frame_ratio_horiz;
+    cfratio_horiz = columns + lmargin + rmargin;
+    canvas_margin_left = lmargin / cfratio_horiz;
+    canvas_margin_right = rmargin / cfratio_horiz;
 
-    canvas_frame_ratio_vert = rows + tmargin + bmargin;
-    canvas_margin_top = tmargin / canvas_frame_ratio_vert;
-    canvas_margin_bottom = bmargin / canvas_frame_ratio_vert;
+    cfratio_vert = rows + tmargin + bmargin;
+    canvas_margin_top = tmargin / cfratio_vert;
+    canvas_margin_bottom = bmargin / cfratio_vert;
 
     if (columns > 1) {
         lmargin_rel = lmargin / (lmargin + 1.0);
         rmargin_rel = rmargin / (rmargin + 1.0);
     } else {
-        lmargin_rel = lmargin / canvas_frame_ratio_horiz;
-        rmargin_rel = rmargin / canvas_frame_ratio_horiz;
+        lmargin_rel = lmargin / cfratio_horiz;
+        rmargin_rel = rmargin / cfratio_horiz;
     }
 
     if (rows > 1) {
         tmargin_rel = tmargin / (tmargin + 1.0);
         bmargin_rel = bmargin / (bmargin + 1.0);
     } else {
-        tmargin_rel = tmargin / canvas_frame_ratio_vert;
-        bmargin_rel = bmargin / canvas_frame_ratio_vert;
+        tmargin_rel = tmargin / cfratio_vert;
+        bmargin_rel = bmargin / cfratio_vert;
     }
 
-    canvas_width = frame_width * canvas_frame_ratio_horiz;
-    canvas_height = frame_height * canvas_frame_ratio_vert;
+    canvas_width = frame_width * cfratio_horiz;
+    canvas_height = frame_height * cfratio_vert;
 
     tile_edges_horiz.clear();
     tile_edges_horiz.push_back(0.0);
     tile_edges_horiz.push_back(canvas_margin_left / lmargin_rel);
     for (int i=2; i<columns; ++i) {
         tile_edges_horiz.push_back(
-            tile_edges_horiz[i-1] + 1.0 / canvas_frame_ratio_horiz
+            tile_edges_horiz[i-1] + 1.0 / cfratio_horiz
         );
     }
     if (columns > 1) { tile_edges_horiz.push_back(1.0); }
 
-    frame_edges_horiz.clear();
+    fedges_horiz.clear();
     tile_widths.clear();
 
-    frame_edges_horiz.push_back(canvas_margin_left);
+    fedges_horiz.push_back(canvas_margin_left);
     for (int i=0; i<columns; ++i) {
-        frame_edges_horiz.push_back(
-            frame_edges_horiz[i] + 1.0 / canvas_frame_ratio_horiz
+        fedges_horiz.push_back(
+            fedges_horiz[i] + 1.0 / cfratio_horiz
         );
         tile_widths.push_back(
             canvas_width *
@@ -205,18 +199,18 @@ void tiling::tile() {
     tile_edges_vert.push_back(1.0 - canvas_margin_top / tmargin_rel);
     for (int i=2; i<rows; ++i) {
         tile_edges_vert.push_back(
-            tile_edges_vert[i-1] - 1.0 / canvas_frame_ratio_vert
+            tile_edges_vert[i-1] - 1.0 / cfratio_vert
         );
     }
     if (rows > 1) { tile_edges_vert.push_back(0.0); }
 
-    frame_edges_vert.clear();
+    fedges_vert.clear();
     tile_heights.clear();
 
-    frame_edges_vert.push_back(1.0 - canvas_margin_top);
+    fedges_vert.push_back(1.0 - canvas_margin_top);
     for (int i=0; i<rows; ++i) {
-        frame_edges_vert.push_back(
-            frame_edges_vert[i] - 1.0 / canvas_frame_ratio_vert
+        fedges_vert.push_back(
+            fedges_vert[i] - 1.0 / cfratio_vert
         );
         tile_heights.push_back(
             canvas_height * (tile_edges_vert[i] - tile_edges_vert[i+1])
@@ -305,7 +299,8 @@ TLatex* tiling::draw_latex_on_frame(float x, float y, const char* text,
     return l1->DrawLatexNDC(x, y, text);
 }
 
-void tiling::set_sizes(TH1* h1, int font,
+template <class T>
+void tiling::set_sizes(T* h1, int font,
                        float x_title_size, float y_title_size,
                        float x_label_size, float y_label_size,
                        float x_tick_size, float y_tick_size,
@@ -313,19 +308,6 @@ void tiling::set_sizes(TH1* h1, int font,
                        float x_label_offset, float y_label_offset,
                        int col, int row) {
     font = font * 10 + 2;
-
-    y_title_size = adjust_size(y_title_size, col, row);
-    y_label_size = adjust_size(y_label_size, col, row);
-    y_tick_size = y_tick_size * tile_heights[row] /frame_height *
-        tile_widths[0] / tile_widths[col];
-    y_title_offset = adjust_size_inverse(y_title_offset, col, row);
-
-    y_title_size = normalize_tile_size(y_title_size);
-    y_label_size = normalize_tile_size(y_label_size);
-    y_tick_size = normalize_tile_size(y_tick_size);
-    y_title_offset = normalize_tile_size_inverse(y_title_offset) *
-        frame_width / tile_widths[0];
-    y_label_offset = y_label_offset * frame_width / tile_widths[0];
 
     x_title_size = adjust_size(x_title_size, col, row);
     x_label_size = adjust_size(x_label_size, col, row);
@@ -346,51 +328,6 @@ void tiling::set_sizes(TH1* h1, int font,
         std::max(tile_heights[0], tile_heights[rows - 1]) * frame_height /
         std::min(tile_heights[0], tile_heights[rows - 1]);
 
-    TAxis* x_axis = h1->GetXaxis();
-    TAxis* y_axis = h1->GetYaxis();
-
-    x_axis->SetTitleFont(font);
-    x_axis->SetTitleSize(x_title_size);
-    x_axis->SetLabelFont(font);
-    x_axis->SetLabelSize(x_label_size);
-    x_axis->SetLabelOffset(x_label_offset);
-    x_axis->SetTickSize(x_tick_size);
-
-    y_axis->SetTitleFont(font);
-    y_axis->SetTitleSize(y_title_size);
-    y_axis->SetLabelFont(font);
-    y_axis->SetLabelSize(y_label_size);
-    y_axis->SetLabelOffset(y_label_offset);
-    y_axis->SetTickSize(y_tick_size);
-
-    if (row == rows - 1) {
-        x_axis->SetTitleOffset(x_title_offset);
-        x_axis->CenterTitle();
-    } else {
-        x_axis->SetTitle("");
-        x_axis->SetTitleOffset(999);
-        x_axis->SetLabelOffset(999);
-    }
-
-    if (col == 0) {
-        y_axis->SetTitleOffset(y_title_offset);
-        y_axis->CenterTitle();
-    } else {
-        y_axis->SetTitle("");
-        y_axis->SetTitleOffset(999);
-        y_axis->SetLabelOffset(999);
-    }
-}
-
-void tiling::set_sizes(TGraph* g1, int font,
-                       float x_title_size, float y_title_size,
-                       float x_label_size, float y_label_size,
-                       float x_tick_size, float y_tick_size,
-                       float x_title_offset, float y_title_offset,
-                       float x_label_offset, float y_label_offset,
-                       int col, int row) {
-    font = font * 10 + 2;
-
     y_title_size = adjust_size(y_title_size, col, row);
     y_label_size = adjust_size(y_label_size, col, row);
     y_tick_size = y_tick_size * tile_heights[row] /frame_height *
@@ -400,22 +337,12 @@ void tiling::set_sizes(TGraph* g1, int font,
     y_title_size = normalize_tile_size(y_title_size);
     y_label_size = normalize_tile_size(y_label_size);
     y_tick_size = normalize_tile_size(y_tick_size);
-    y_title_offset = normalize_tile_size_inverse(y_title_offset);
+    y_title_offset = normalize_tile_size_inverse(y_title_offset) *
+        frame_width / tile_widths[0];
+    y_label_offset = y_label_offset * frame_width / tile_widths[0];
 
-    x_title_size = adjust_size(x_title_size, col, row);
-    x_label_size = adjust_size(x_label_size, col, row);
-    x_tick_size = x_tick_size * tile_widths[col] / frame_width *
-        tile_widths[0] / tile_heights[0];
-    x_title_offset = adjust_size_inverse(x_title_offset, col, row);
-    x_label_offset = x_label_offset - x_label_size * 0.8;
-
-    x_title_size = normalize_tile_size(x_title_size);
-    x_label_size = normalize_tile_size(x_label_size);
-    x_tick_size = normalize_tile_size(x_tick_size);
-    x_label_offset = normalize_tile_size(x_label_offset);
-
-    TAxis* x_axis = g1->GetXaxis();
-    TAxis* y_axis = g1->GetYaxis();
+    TAxis* x_axis = h1->GetXaxis();
+    TAxis* y_axis = h1->GetYaxis();
 
     x_axis->SetTitleFont(font);
     x_axis->SetTitleSize(x_title_size);
@@ -476,24 +403,17 @@ void tiling::cover_axis_labels(int x_options, int y_options,
     float x_min, x_max;
     float y_min, y_max;
 
-    x_label_size = adjust_size(x_label_size, 0, 0);
-    x_label_offset = x_label_offset - x_label_size * 0.8 + 0.06;
-    x_label_size = normalize_tile_size(x_label_size);
-    x_label_offset = normalize_tile_size(x_label_offset);
-
-    y_label_size = normalize_tile_size(adjust_size(y_label_size, 0, 0));
-    y_label_offset = y_label_offset * frame_width / tile_widths[0];
-
-    y_max = frame_edges_vert[rows] - x_label_offset;
-    y_min = y_max - x_label_size;
+    y_max = std::max(0.f, fedges_vert[rows] - x_label_offset / cfratio_vert);
+    y_min = std::max(0.f, y_max - x_label_size / cfratio_vert);
 
     for (int i = columns - 1; i >= 0 && x_options != 0; --i, x_options /= 10) {
         int opt = x_options % 10;
 
         if (opt & 0x1) {
-            x_min = frame_edges_horiz[i];
-            x_max = x_min + x_label_size;
-            if (i == 0) { x_min -= x_label_size; }
+            x_min = fedges_horiz[i];
+            x_max = x_min + x_label_size / cfratio_horiz;
+            if (i == 0) { x_min = std::max(0.f,
+                x_min - 2 * x_label_size / cfratio_horiz); }
 
             TPad* cover = new TPad(
                 Form("x_cover_left_%i", x_options),
@@ -505,9 +425,10 @@ void tiling::cover_axis_labels(int x_options, int y_options,
         }
 
         if (opt & 0x2) {
-            x_max = frame_edges_horiz[i + 1];
-            x_min = x_max - x_label_size;
-            if (i == columns - 1) { x_max += x_label_size; }
+            x_max = fedges_horiz[i + 1];
+            x_min = x_max - x_label_size / cfratio_horiz;
+            if (i == columns - 1) { x_max = std::min(1.f,
+                x_max + 2 * x_label_size / cfratio_horiz); }
 
             TPad* cover = new TPad(
                 Form("x_cover_right_%i", x_options),
@@ -519,16 +440,17 @@ void tiling::cover_axis_labels(int x_options, int y_options,
         }
     }
 
-    x_max = frame_edges_horiz[0] - y_label_offset;
-    x_min = x_max - y_label_size;
+    x_max = std::max(0.f, fedges_horiz[0] - y_label_offset / cfratio_horiz);
+    x_min = std::max(0.f, x_max - 4 * y_label_size / cfratio_horiz);
 
     for (int i = rows - 1; i >= 0 && y_options != 0; --i, y_options /= 10) {
         int opt = y_options % 10;
 
         if (opt & 0x1) {
-            y_max = frame_edges_vert[i];
-            y_min = y_max - y_label_size;
-            if (i == 0) { y_max += y_label_size; }
+            y_max = fedges_vert[i];
+            y_min = y_max - y_label_size / cfratio_vert;
+            if (i == 0) { y_max = std::min(1.f,
+                y_max + 2 * y_label_size / cfratio_vert); }
 
             TPad* cover = new TPad(
                 Form("x_cover_top_%i", y_options),
@@ -540,9 +462,10 @@ void tiling::cover_axis_labels(int x_options, int y_options,
         }
 
         if (opt & 0x2) {
-            y_min = frame_edges_vert[i + 1];
-            y_max = y_min + y_label_size;
-            if (i == rows - 1) { y_min -= y_label_size; }
+            y_min = fedges_vert[i + 1];
+            y_max = y_min + y_label_size / cfratio_vert;
+            if (i == rows - 1) { y_min = std::max(0.f,
+                y_min - 2 * y_label_size / cfratio_vert); }
 
             TPad* cover = new TPad(
                 Form("x_cover_bottom_%i", y_options),
