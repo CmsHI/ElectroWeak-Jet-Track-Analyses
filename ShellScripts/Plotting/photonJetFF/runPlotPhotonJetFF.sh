e0011f9dd429fdcec4e84b54e0750fb7d854fdcd
Kaya Tatar <tatark@mit.edu>
2017-08-08 16:01:21 -0400

    add shell script for making result figures

---
+++ ShellScripts/Plotting/photonJetFF/runPlotPhotonJetFF.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -x
+
+g++ Plotting/photonJetFF/plotPhotonJetFF.C $(root-config --cflags --libs) -Werror -Wall -Wextra -Wno-narrowing -O2 -o Plotting/photonJetFF/plotPhotonJetFF.exe
+
+progPath="./Plotting/photonJetFF/plotPhotonJetFF.exe"
+inputFiles=(
+"./Data/photonJetFF/data_60_30_gxi0_defnFF1-final-and-systematics.root"
+"./Data/photonJetFF/data_60_30_gxi1_defnFF1-final-and-systematics.root"
+"./Data/photonJetFF/data_80_40_gxi0_defnFF1-final-and-systematics.root"
+"./Data/photonJetFF/data_80_40_gxi1_defnFF1-final-and-systematics.root"
+"./Data/photonJetFF/data_60_30_gxi0_defnFF1-final-and-systematics.root"
+"./Data/photonJetFF/data_60_30_gxi1_defnFF1-final-and-systematics.root"
+"./Data/photonJetFF/data_60_30_gxi0_defnFF1-final-and-systematics.root"
+"./Data/photonJetFF/data_60_30_gxi1_defnFF1-final-and-systematics.root"
+);
+
+histLists=(
+"Configurations/photonJetFF/ff.list"
+"Configurations/photonJetFF/ff.list"
+"Configurations/photonJetFF/ff.list"
+"Configurations/photonJetFF/ff.list"
+"Configurations/photonJetFF/ff-cent030.list"
+"Configurations/photonJetFF/ff-cent030.list"
+"Configurations/photonJetFF/ff-reweight.list"
+"Configurations/photonJetFF/ff-reweight.list"
+);
+
+configList=(
+"Configurations/photonJetFF/ff-60-30-gxi0.conf"
+"Configurations/photonJetFF/ff-60-30-gxi1.conf"
+"Configurations/photonJetFF/ff-80-40-gxi0.conf"
+"Configurations/photonJetFF/ff-80-40-gxi1.conf"
+"Configurations/photonJetFF/ff-60-30-gxi0-cent030.conf"
+"Configurations/photonJetFF/ff-60-30-gxi1-cent030.conf"
+"Configurations/photonJetFF/ff-60-30-gxi0-reweight.conf"
+"Configurations/photonJetFF/ff-60-30-gxi1-reweight.conf"
+);
+
+arrayIndices=${!configList[*]}
+for i1 in $arrayIndices
+do
+    configFile=${configList[i1]}
+    outputFileLOG="${configFile/.conf/.log}"
+    inputFile=${inputFiles[i1]}
+    histList=${histLists[i1]}
+    $progPath $inputFile $histList $configFile &> $outputFileLOG
+done
+
