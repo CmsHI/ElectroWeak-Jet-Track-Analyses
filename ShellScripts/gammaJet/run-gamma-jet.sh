1fa7efaad1f79245f1a8ca091c5ff765cfee8699
Ran Bi <biran@mit.edu>
2017-08-23 06:25:07 -0400

    update gamma-jet script

--- ShellScripts/gammaJet/run-gamma-jet.sh
+++ ShellScripts/gammaJet/run-gamma-jet.sh
@@ -21,34 +21,32 @@ INFILE=(./ShellScripts/gammaJet/PbPb_Data_HiForest.list ./ShellScripts/gammaJet/
 MIXINGSAMPLE=(${MIXDIR}/PbPb_Data_minbiasJetSkim.root ${MIXDIR}/PbPb_MC_minbiasJetSkim.root DUMMY.root DUMMY.root)
 NJOBS=(400 100 200 5)
 
-SYS_SUFFIX=("" _PES _PURITY_UP _PURITY_DOWN _ELE_REJ _ISO _JER _JES_UP _JES_DOWN)
+SYS_SUFFIX=("" _PES _PURITY_UP _PURITY_DOWN _ELE_REJ _ISO _JER _JES_UP _JES_DOWN _QUARK _GLUON)
 SAM_SUFFIX=("" _mc _pp _pp_mc)
 
 if [[ $3 -eq 0 ]]; then
     for ISAM in ${!SAM_SUFFIX[@]}
     do
-        CONFSUFFIX=${SAM_SUFFIX[ISAM]}${SYS_SUFFIX[ISYS]}
-        ./ShellScripts/gammaJet/gamma-jet-condor.sh ./CutConfigurations/gammaJet${CONFSUFFIX}.conf ${INFILE[ISAM]} $HADOOPDIR/${SAMPLE[ISAM]}_unmerged${SYS_SUFFIX[ISYS]}/ ${NJOBS[ISAM]} ${MIXINGSAMPLE[ISAM]}
+        ./ShellScripts/gammaJet/gamma-jet-condor.sh ./CutConfigurations/gammaJet${SAM_SUFFIX[ISAM]}.conf ${INFILE[ISAM]} $HADOOPDIR/${SAMPLE[ISAM]}_unmerged/ ${NJOBS[ISAM]} ${MIXINGSAMPLE[ISAM]}
     done
 fi
 
-for ISYS in ${!SYS_SUFFIX[@]}
-do
-    if [[ $3 -eq 1 ]]; then
-        for ISAM in ${!SAM_SUFFIX[@]}
-        do
-            # hadd $OUTDIR/${SAMPLE[ISAM]}_gammaJetSkim.root $HADOOPDIR/${SAMPLE[ISAM]}_unmerged/gammaJetSkim*.root &
-            hadd $OUTDIR/${SAMPLE[ISAM]}_gammaJetHistogram${SYS_SUFFIX[ISYS]}.root $HADOOPDIR/${SAMPLE[ISAM]}_unmerged${SYS_SUFFIX[ISYS]}/gammaJetHistogram*.root &
-        done
-        wait
+if [[ $3 -eq 1 ]]; then
+    for ISAM in ${!SAM_SUFFIX[@]}
+    do
+        hadd $OUTDIR/${SAMPLE[ISAM]}_gammaJetSkim.root $HADOOPDIR/${SAMPLE[ISAM]}_unmerged/gammaJetSkim*.root &
+    done
+    wait
 
+    for ISYS in ${!SYS_SUFFIX[@]}
+    do
         for ISAM in ${!SAM_SUFFIX[@]}
         do
-            CONFSUFFIX=${SAM_SUFFIX[ISAM]}
+            CONFSUFFIX=${SAM_SUFFIX[ISAM]}${SYS_SUFFIX[ISYS]}
             ./Histogramming/gammaJetHistogramArithmetic.exe ./CutConfigurations/gammaJet${CONFSUFFIX}.conf $OUTDIR/${SAMPLE[ISAM]}_gammaJetHistogram${SYS_SUFFIX[ISYS]}.root $OUTDIR/${SAMPLE[ISAM]}_gammaJetHistogramArithmetic${SYS_SUFFIX[ISYS]}.root &> $OUTDIR/${SAMPLE[ISAM]}_gammaJetHistogramArithmetic${SYS_SUFFIX[ISYS]}.log &
         done
         wait
 
         ./Histogramming/gammaJetFinalHistograms.exe ./CutConfigurations/gammaJet.conf ${OUTDIR}/PbPb_Data_gammaJetHistogramArithmetic${SYS_SUFFIX[ISYS]}.root ${OUTDIR}/PbPb_MC_gammaJetHistogramArithmetic${SYS_SUFFIX[ISYS]}.root ${OUTDIR}/pp_Data_gammaJetHistogramArithmetic${SYS_SUFFIX[ISYS]}.root ${OUTDIR}/pp_MC_gammaJetHistogramArithmetic${SYS_SUFFIX[ISYS]}.root ${OUTDIR}/gammaJetHistograms${SYS_SUFFIX[ISYS]}.root &> ${OUTDIR}/gammaJetHistograms${SYS_SUFFIX[ISYS]}.log
-    fi
-done
+    done
+fi
