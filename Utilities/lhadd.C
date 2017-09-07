18281c9c2f116c592080dbfb8b2827570656d2d4
Kaya Tatar <tatark@mit.edu>
2017-09-07 09:17:25 -0400

    print lines to track the merging steps

--- Utilities/lhadd.C
+++ Utilities/lhadd.C
@@ -2,10 +2,15 @@
 #include "TTree.h"
 
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
 
 int lhadd(std::string input, std::string output) {
+    std::cout<<"running lhadd()"<<std::endl;
+    std::cout<<"input list  = "<< input.c_str() <<std::endl;
+    std::cout<<"output      = "<< output.c_str() <<std::endl;
+
     TTree::SetMaxTreeSize(1LL * 1024 * 1024 * 1024 * 1024);
 
     std::ifstream file_stream(input);
@@ -16,16 +21,33 @@ int lhadd(std::string input, std::string output) {
     while (std::getline(file_stream, line))
         file_list.push_back(line);
 
+    int nFiles = file_list.size();
+    std::cout<<"input ROOT files : num = "<< nFiles << std::endl;
+    std::cout<<"#####"<< std::endl;
+    for (std::vector<std::string>::iterator it = file_list.begin() ; it != file_list.end(); ++it) {
+        std::cout<<(*it).c_str()<< std::endl;
+    }
+    std::cout<<"##### END #####"<< std::endl;
+
     TFileMerger merger(kFALSE, kFALSE);
     merger.OutputFile(output.c_str());
-    for (std::size_t i = 0; i < file_list.size(); ++i) {
+    std::cout<< "adding files" <<std::endl;
+    for (int i = 0; i < nFiles; ++i) {
+        std::string filePath = file_list.at(i).c_str();
+        std::cout <<"i = " << i << " , " ;
+        std::cout <<"file : " << filePath.c_str() << std::endl;
         if (!merger.AddFile(file_list[i].c_str())) {
+            std::cout << "failed adding file : " << filePath.c_str() << std::endl;
             return 1;
         }
     }
+    std::cout<< "adding files - END" <<std::endl;
 
+    std::cout<< "merging" <<std::endl;
     merger.Merge();
+    std::cout<< "merging - END" <<std::endl;
 
+    std::cout<< "running lhadd() - END" <<std::endl;
     return 0;
 }
 
