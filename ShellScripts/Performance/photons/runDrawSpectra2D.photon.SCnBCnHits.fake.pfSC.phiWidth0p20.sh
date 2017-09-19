5bb0c11c8e7a45a0d3a169a47cd067b51dcc85e6
Kaya Tatar <tatark@mit.edu>
2017-09-19 19:20:25 -0400

    add shell script to run spectra2D.photon.SCnBC.conf and spectra2D.photon.SCnBCnHits.fake.conf configurations for AllQCD photon samples using 1. defult GED reco 2. GED reco with smaller SC phiWidth search window

---
+++ ShellScripts/Performance/photons/runDrawSpectra2D.photon.SCnBCnHits.fake.pfSC.phiWidth0p20.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#g++ -Wall -Werror -Wextra -O2 -Wno-narrowing `root-config --cflags --libs` -lTMVA -lRooFitCore -lRooFit -MMD -MF ./build/Performance/drawSpectra.d Performance/drawSpectra.C -o Performance/drawSpectra.exe
+
+runCmd="time -p"
+runCmdStr="time -p"
+if [[ $USER == "tatar" ]]; then
+  runCmd="$HOME/code/scripts/myRun.sh"
+  runCmdStr="myRun"
+fi
+
+progPath="./Performance/drawSpectra.exe"
+inputList=(
+"Configurations/filelists/Pythia8_AllQCDPhoton30_Hydjet_Cymbal_MB_FOREST_extendEC.list"
+"Configurations/filelists/Pythia8_AllQCDPhoton30_Hydjet_Cymbal_MB_FOREST_extendEC.list"
+"Configurations/filelists/Pythia8_AllQCDPhoton30_Hydjet_Cymbal_MB_FOREST_extendEC_pfSC_phiWidth0p20.list"
+);
+
+configFiles=(
+"Configurations/photons/spectra2D.photon.SCnBCnHits.fake.conf"
+"Configurations/photons/spectra2D.photon.SCnBCnHits.fake.GED.conf"
+"Configurations/photons/spectra2D.photon.SCnBCnHits.fake.GED.conf"
+);
+
+outDirBase="/export/d00/scratch/"$USER"/EWJTA-out/"
+outputSuffix="Pythia8_AllQCDPhoton30_Hydjet_Cymbal_MB"
+outputFiles=(
+$outDirBase"Configurations/photons/spectra2D_photon_SCnBCnHits_fake_"$outputSuffix"_extendEC.root"
+$outDirBase"Configurations/photons/spectra2D_photon_SCnBCnHits_fake_GED_"$outputSuffix"_extendEC.root"
+$outDirBase"Configurations/photons/spectra2D_photon_SCnBCnHits_fake_GED_"$outputSuffix"_extendEC_phiWidth0p20.root"
+);
+
+arrayIndices=${!outputFiles[*]}
+for i1 in $arrayIndices
+do
+    configFile=${configFiles[i1]}
+    inputFile=${inputList[i1]}
+    outputFile=${outputFiles[i1]}
+    outputFileLOG="${outputFile/.root/.log}"
+    outDir=$(dirname "${outputFile}")
+    mkdir -p $outDir
+    $runCmd $progPath $configFile $inputFile $outputFile &> $outputFileLOG &
+    echo "$runCmdStr $progPath $configFile $inputFile $outputFile &> $outputFileLOG &"
+done
+
+
