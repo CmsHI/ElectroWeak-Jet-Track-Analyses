ba9272504c061699723f6eaff00d4530720a0cd7
Kaya Tatar <tatark@mit.edu>
2017-11-13 00:12:54 -0500

    add shell script to run photonTriggerAna on HIRun2017PP Run3065XX data. add file list for ExpressForests.

---
+++ ShellScripts/Performance/photons/HIRun2017PP/runPhotonTriggerAna.ExpressForests.Run3065XX.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+runCmd="./ShellScripts/myRun.sh"
+progPath="./Performance/photons/photonTriggerAna.exe"
+hltFiles=(
+"DUMMY.root"
+"DUMMY.root"
+)
+inputFile="Configurations/filelists/HIRun2017PP_ExpressForests_Run3065XX.list"
+
+userInitial=${USER:0:1}
+outDirBase="/afs/cern.ch/work/"$userInitial"/"$USER"/public/code/ElectroWeak-Jet-Track-Analyses/EWJTA-out/"
+outputSuffix="ExpressForests_Run3065XX"
+outList=(
+$outDirBase"Performance/photons/HIRun2017PP/photonTriggerAna_"$outputSuffix".root"
+$outDirBase"Performance/photons/HIRun2017PP/photonTriggerAna_"$outputSuffix"_HoverELoose.root"
+);
+
+configList=(
+"Configurations/Performance/photons/HIRun2017PP/photonTriggerAna.ExpressForests.Run3065XX.conf"
+"Configurations/Performance/photons/HIRun2017PP/photonTriggerAna.ExpressForests.Run3065XX.HoverELoose.conf"
+);
+
+arrayIndices=${!outList[*]}
+for i1 in $arrayIndices
+do
+    configFile=${configList[i1]}
+    hltFile=${hltFiles[i1]}
+    outputFile=${outList[i1]}
+    outputFileLOG="${outputFile/.root/.log}"
+    outDir=$(dirname "${outputFile}")
+    mkdir -p $outDir
+    $runCmd $progPath $configFile $hltFile $inputFile $outputFile &> $outputFileLOG &
+    echo "$runCmd $progPath $configFile $hltFile $inputFile $outputFile &> $outputFileLOG &"
+done
+
