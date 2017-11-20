7e69c4fcbe45630ab2fc06f223bce610c613a949
Kaya Tatar <tatark@mit.edu>
2017-11-20 17:58:28 -0500

    add HighEGJet Prompt Reco forest for Run306631, use MatchHltObj mode for efficiency

--- ShellScripts/Performance/photons/HIRun2017PP/runPhotonTriggerAna.Run3065XX.sh
+++ ShellScripts/Performance/photons/HIRun2017PP/runPhotonTriggerAna.Run3065XX.sh
@@ -12,7 +12,9 @@ hltFiles=(
 #inputFile="Configurations/filelists/HIRun2017PP/ExpressForests_Run306572.list"
 #inputFile="Configurations/filelists/HIRun2017PP/ExpressForests_Run306580.list"
 #inputFile="Configurations/filelists/HIRun2017PP/Prompt_Reco_HighEGJet_Run306563.list"
-inputFile="Configurations/filelists/HIRun2017PP/Prompt_Reco_HighEGJet_Run306572.list"
+#inputFile="Configurations/filelists/HIRun2017PP/Prompt_Reco_HighEGJet_Run306572.list"
+#inputFile="Configurations/filelists/HIRun2017PP/Prompt_Reco_HighEGJet_Run306631.list"
+inputFile="Configurations/filelists/HIRun2017PP/Prompt_Reco_HighEGJet_Run306631.list"
 
 userInitial=${USER:0:1}
 outDirBase="/afs/cern.ch/work/"$userInitial"/"$USER"/public/code/ElectroWeak-Jet-Track-Analyses/EWJTA-out/"
@@ -22,7 +24,9 @@ outDirBase="/afs/cern.ch/work/"$userInitial"/"$USER"/public/code/ElectroWeak-Jet
 #outputSuffix="ExpressForests_Run306572_denomL1_nonGED"
 #outputSuffix="ExpressForests_Run306580_denomL1"
 #outputSuffix="Prompt_Reco_HighEGJet_Run306563_denomL1"
-outputSuffix="Prompt_Reco_HighEGJet_Run306572_denomL1"
+#outputSuffix="Prompt_Reco_HighEGJet_Run306572_denomL1"
+#outputSuffix="Prompt_Reco_HighEGJet_Run306631_denomL1"
+outputSuffix="Prompt_Reco_HighEGJet_Run306631_denomL1_matchHLT"
 outList=(
 $outDirBase"Performance/photons/HIRun2017PP/photonTriggerAna_"$outputSuffix".root"
 $outDirBase"Performance/photons/HIRun2017PP/photonTriggerAna_"$outputSuffix"_HoverELoose.root"
