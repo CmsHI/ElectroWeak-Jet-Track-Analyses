aaa2dda97764f601df5388e3a62f6a6a81c77c1f
Kaya Tatar <tatark@mit.edu>
2017-12-28 21:54:41 +0300

    further fixes for compilation

--- Plotting/photonJetFF/compact/plotUtils.h
+++ Plotting/photonJetFF/compact/plotUtils.h
@@ -2,8 +2,8 @@
 #include <TLatex.h>
 
 static  int      myDarkRed     = TColor::GetColor(128,0,0);
-static  int      myDarkGreen   = TColor::GetColor(0,128,0);
-static  int      myDarkBlue    = TColor::GetColor(0,0,128);
+//static  int      myDarkGreen   = TColor::GetColor(0,128,0);
+//static  int      myDarkBlue    = TColor::GetColor(0,0,128);
 
 TH1F *DrawFrame(Double_t xmin, Double_t xmax, Double_t ymin, Double_t ymax, TString xTitle = "", TString yTitle = "", bool setMargins = true) {
 
