180337a26ede1179a7e8f752902093c87bcc3e60
Kaya Tatar <tatark@mit.edu>
2018-01-15 16:11:06 -0500

    add shell scripts to calculate pthat weights for official Pythia8 AllQCDPhoton Hydjet Cymbal MB samples, uses the previously produced pthat spectra histograms.

---
+++ ShellScripts/Corrections/event/runCalcPtHatWeights.AllQCDPhoton.HydjetCymbalMB.HINPbPbWinter16DR.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+## First run the following script so that the input for this script is produced :
+## ./ShellScripts/event/runDrawSpectra.pthat.AllQCDPhoton.HydjetCymbalMB.HINPbPbWinter16DR.sh
+
+runCmd="./ShellScripts/myRun.sh"
+progPath="./Corrections/calcPtHatWeights.exe"
+
+outDirBase=$EWJTAOUT
+if [ -z "$outDirBase" ]; then
+  outDirBase="/export/d00/scratch/"$USER"/EWJTA-out"
+fi
+
+file1=$outDirBase"/Configurations/event/spectra_pthat_AllQCDPhoton15_Hydjet_Cymbal_MB_HINPbPbWinter16DR.root"
+file2=$outDirBase"/Configurations/event/spectra_pthat_AllQCDPhotonXX_Hydjet_Cymbal_MB_HINPbPbWinter16DR.root"
+
+hist1="h_0"
+hist2="h_0"
+
+ptHatMins=(15 30 50 80  120);
+ptHatMaxs=(30 50 80 120 -1);
+
+arrayIndices=${!ptHatMins[*]}
+for i1 in $arrayIndices
+do
+    ptHatMin=${ptHatMins[i1]}
+    ptHatMax=${ptHatMaxs[i1]}
+    $runCmd $progPath $file1 $file2 $hist1 $hist2 $ptHatMin $ptHatMax
+done
