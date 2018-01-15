f489ffa2f8a837d5d8c434bba5c146e00f1e0609
Kaya Tatar <tatark@mit.edu>
2018-01-14 21:21:36 -0500

    add spectra configuration and shell scripts for pthat spectra in official Pythia8 AllQCDPhoton samples

---
+++ ShellScripts/event/runDrawSpectra.pthat.AllQCDPhoton.HINppWinter16DR.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#g++ -Wall -Werror -Wextra -O2 -Wno-narrowing `root-config --cflags --libs` -lTMVA -lRooFitCore -lRooFit -MMD -MF ./build/Performance/drawSpectra.d Performance/drawSpectra.C -o Performance/drawSpectra.exe
+
+runCmd="./ShellScripts/myRun.sh"
+progPath="./Performance/drawSpectra.exe"
+inputFiles=(
+"Configurations/filelists/Pythia8_Photon15_pp502_TuneCUETP8M1_HINppWinter16DR_FOREST.list"
+"Configurations/filelists/Pythia8_Photon30_pp502_TuneCUETP8M1_HINppWinter16DR_FOREST.list"
+"Configurations/filelists/Pythia8_Photon50_pp502_TuneCUETP8M1_HINppWinter16DR_FOREST.list"
+"Configurations/filelists/Pythia8_Photon80_pp502_TuneCUETP8M1_HINppWinter16DR_FOREST.list"
+"Configurations/filelists/Pythia8_Photon120_pp502_TuneCUETP8M1_HINppWinter16DR_FOREST.list"
+"Configurations/filelists/Pythia8_PhotonXX_pp502_TuneCUETP8M1_HINppWinter16DR_FOREST.list"
+);
+
+configFiles=(
+"Configurations/event/Pythia8_AllQCDPhoton_HINppWinter16DR/spectra.pthat.conf"
+"Configurations/event/Pythia8_AllQCDPhoton_HINppWinter16DR/spectra.pthat.conf"
+"Configurations/event/Pythia8_AllQCDPhoton_HINppWinter16DR/spectra.pthat.conf"
+"Configurations/event/Pythia8_AllQCDPhoton_HINppWinter16DR/spectra.pthat.conf"
+"Configurations/event/Pythia8_AllQCDPhoton_HINppWinter16DR/spectra.pthat.conf"
+"Configurations/event/Pythia8_AllQCDPhoton_HINppWinter16DR/spectra.pthat.conf"
+);
+
+outDirBase="/export/d00/scratch/"$USER"/EWJTA-out/"
+outputFiles=(
+$outDirBase"Configurations/event/spectra_pthat_Photon15_pp502_TuneCUETP8M1_HINppWinter16DR.root"
+$outDirBase"Configurations/event/spectra_pthat_Photon30_pp502_TuneCUETP8M1_HINppWinter16DR.root"
+$outDirBase"Configurations/event/spectra_pthat_Photon50_pp502_TuneCUETP8M1_HINppWinter16DR.root"
+$outDirBase"Configurations/event/spectra_pthat_Photon80_pp502_TuneCUETP8M1_HINppWinter16DR.root"
+$outDirBase"Configurations/event/spectra_pthat_Photon120_pp502_TuneCUETP8M1_HINppWinter16DR.root"
+$outDirBase"Configurations/event/spectra_pthat_PhotonXX_pp502_TuneCUETP8M1_HINppWinter16DR.root"
+);
+
+arrayIndices=${!outputFiles[*]}
+for i1 in $arrayIndices
+do
+    inputFile=${inputFiles[i1]}
+    configFile=${configFiles[i1]}
+    outputFile=${outputFiles[i1]}
+    outputFileLOG="${outputFile/.root/.log}"
+    outDir=$(dirname "${outputFile}")
+    mkdir -p $outDir
+    $runCmd $progPath $configFile $inputFile $outputFile &> $outputFileLOG &
+    echo "$runCmd $progPath $configFile $inputFile $outputFile &> $outputFileLOG &"
+done
+
+
