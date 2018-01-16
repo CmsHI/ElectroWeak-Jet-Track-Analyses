3ff857af59fff1844ad792738b2118ca1e58255a
Kaya Tatar <tatark@mit.edu>
2018-01-16 14:25:01 -0500

    add xi phase space plots to the list of supplementary figures for PAS

--- ShellScripts/Plotting/photonJetFF/cp2pasHIN16014supplementary.sh
+++ ShellScripts/Plotting/photonJetFF/cp2pasHIN16014supplementary.sh
@@ -15,6 +15,15 @@ sourceFiles=(
 "plotTechnicalTrk.pdf"
 "plotTechnicalJet.pdf"
 "plotTechnicalSumIso.pdf"
+### xi phase space
+"Plotting/photonJetFF/jetpt_vs_xi.pdf"
+"Plotting/photonJetFF/trackpt_vs_xi.pdf"
+"Plotting/photonJetFF/jetpt_vs_xi_plusminus_trkPt1_deta0p3.pdf"
+"Plotting/photonJetFF/trackpt_vs_xi_plusminus_jetPt30_deta0p3.pdf"
+"Plotting/photonJetFF/phopt_vs_xi.pdf"
+"Plotting/photonJetFF/trackpt_vs_xigamma.pdf"
+"Plotting/photonJetFF/phopt_vs_xi_trkPt1.pdf"
+"Plotting/photonJetFF/trackpt_vs_xigamma_phoPt60.pdf"
 );
 
 destinationFiles=(
@@ -23,6 +32,15 @@ destinationFiles=(
 "figures/supplementary/xijet_bkgTrk_subtraction.pdf"
 "figures/supplementary/xijet_bkgJet_subtraction.pdf"
 "figures/supplementary/sumIso_Pythia_Hydjet_sigbkg.pdf"
+### xi phase space
+"figures/supplementary/fnc_jetpt_vs_xijet.pdf"
+"figures/supplementary/fnc_trackpt_vs_xijet.pdf"
+"figures/supplementary/fnc_jetpt_vs_xijet_trkPt1_deta0p3.pdf"
+"figures/supplementary/fnc_trackpt_vs_xijet_jetPt30_deta0p3.pdf"
+"figures/supplementary/fnc_phopt_vs_xigamma.pdf"
+"figures/supplementary/fnc_trackpt_vs_xigamma.pdf"
+"figures/supplementary/fnc_phopt_vs_xigamma_trkPt1.pdf"
+"figures/supplementary/fnc_trackpt_vs_xigamma_phoPt60.pdf"
 );
 
 arrayIndices=${!sourceFiles[*]}
