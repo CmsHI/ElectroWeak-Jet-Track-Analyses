feed362a9853425544b4edb6e53a9b4f3088f983
Kaya Tatar <tatark@mit.edu>
2018-01-31 23:24:01 -0500

    update shell scripts with samples corresponding to different 0.15 and 0.10 WP for SC phiWidth

--- ShellScripts/Performance/photons/runDrawSpectra2D.photon.SCnBC.SCnHits.pfSC.phiWidth0pXX.sh
+++ ShellScripts/Performance/photons/runDrawSpectra2D.photon.SCnBC.SCnHits.pfSC.phiWidth0pXX.sh
@@ -7,6 +7,8 @@ progPath="./Performance/drawSpectra.exe"
 inputList=(
 "Configurations/filelists/photons/Pythia8_AllQCDPhoton30_Hydjet_Cymbal_MB_FOREST_extendEC.list"
 "Configurations/filelists/photons/Pythia8_AllQCDPhoton30_Hydjet_Cymbal_MB_FOREST_extendEC.list"
+"Configurations/filelists/photons/Pythia8_AllQCDPhoton30_Hydjet_Cymbal_MB_FOREST_extendEC_pfSC_phiWidth0p10.list"
+"Configurations/filelists/photons/Pythia8_AllQCDPhoton30_Hydjet_Cymbal_MB_FOREST_extendEC_pfSC_phiWidth0p15.list"
 "Configurations/filelists/photons/Pythia8_AllQCDPhoton30_Hydjet_Cymbal_MB_FOREST_extendEC_pfSC_phiWidth0p20.list"
 "Configurations/filelists/photons/Pythia8_AllQCDPhoton30_Hydjet_Cymbal_MB_FOREST_extendEC_pfSC_phiWidth0p30.list"
 "Configurations/filelists/photons/Pythia8_AllQCDPhoton30_Hydjet_Cymbal_MB_FOREST_extendEC_pfSC_phiWidth0p40.list"
@@ -14,6 +16,8 @@ inputList=(
 
 configFiles=(
 "Configurations/photons/spectra2D.photon.SCnBC.conf"
+"Configurations/photons/spectra2D.photon.SCnBC.GED.conf"
+"Configurations/photons/spectra2D.photon.SCnBC.GED.conf"
 "Configurations/photons/spectra2D.photon.SCnBC.GED.conf"
 "Configurations/photons/spectra2D.photon.SCnBC.GED.conf"
 "Configurations/photons/spectra2D.photon.SCnBC.GED.conf"
@@ -28,6 +32,8 @@ outputSuffix="Pythia8_AllQCDPhoton30_Hydjet_Cymbal_MB"
 outputFiles=(
 $outDirBase"/Configurations/photons/spectra2D_photon_SCnBC_"$outputSuffix"_extendEC.root"
 $outDirBase"/Configurations/photons/spectra2D_photon_SCnBC_GED_"$outputSuffix"_extendEC.root"
+$outDirBase"/Configurations/photons/spectra2D_photon_SCnBC_GED_"$outputSuffix"_extendEC_phiWidth0p10.root"
+$outDirBase"/Configurations/photons/spectra2D_photon_SCnBC_GED_"$outputSuffix"_extendEC_phiWidth0p15.root"
 $outDirBase"/Configurations/photons/spectra2D_photon_SCnBC_GED_"$outputSuffix"_extendEC_phiWidth0p20.root"
 $outDirBase"/Configurations/photons/spectra2D_photon_SCnBC_GED_"$outputSuffix"_extendEC_phiWidth0p30.root"
 $outDirBase"/Configurations/photons/spectra2D_photon_SCnBC_GED_"$outputSuffix"_extendEC_phiWidth0p40.root"
