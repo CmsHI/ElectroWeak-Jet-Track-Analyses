b26ee9422bad4bb206dd8ef68bdef1ff274ad5eb
Kaya Tatar <tatark@mit.edu>
2018-02-20 23:40:42 -0500

    configurations to run photonTriggerAna for L1 EG objects on HIPhoton40AndZ_HIRun2015_PromptReco_v1_HLTfilter_SinglePhoton_FOREST. The analysis type is L1Objects, uses L1NTuples produced with default Emulation setup and with egBypassEGVetos=1 setup.

--- ShellScripts/Performance/photons/HIRun2018PbPb/L1T/runPhotonTriggerAna.L1T.HIRun2015.PbPb.HIPhoton40AndZ.sh
+++ ShellScripts/Performance/photons/HIRun2018PbPb/L1T/runPhotonTriggerAna.L1T.HIRun2015.PbPb.HIPhoton40AndZ.sh
@@ -4,6 +4,8 @@ runCmd="./ShellScripts/myRun.sh"
 progPath="./Performance/photons/photonTriggerAna.exe"
 triggerFiles=(
 "/mnt/hadoop/cms/store/user/katatar/HIRun2018PbPb/L1T/L1Ntuple_HIRun2015_HIPhoton40AndZ/180218_063756/L1Ntuple_merged_v2.root"
+"/mnt/hadoop/cms/store/user/katatar/HIRun2018PbPb/L1T/L1Ntuple_HIRun2015_HIPhoton40AndZ/180218_063756/L1Ntuple_merged_v2.root"
+"/mnt/hadoop/cms/store/user/katatar/HIRun2018PbPb/L1T/L1Ntuple_HIRun2015_HIPhoton40AndZ_egBypassEGVetos1/180218_063831/L1Ntuple_merged_v2.root"
 )
 inputFile="Configurations/filelists/HIPhoton40AndZ_HIRun2015_PromptReco_v1_HLTfilter_SinglePhoton_FOREST_Run262XXX.list"
 ## create the file list on the fly
@@ -20,10 +22,14 @@ fi
 outputSuffix="HIPhoton40AndZ_HIRun2015_PromptReco_v1_HLTfilter_SinglePhoton"
 outList=(
 $outDirBase"/Performance/photons/HIRun2018PbPb/L1T/photonTriggerAna_"$outputSuffix"_L1SingleEG.root"
+$outDirBase"/Performance/photons/HIRun2018PbPb/L1T/photonTriggerAna_"$outputSuffix"_L1T.root"
+$outDirBase"/Performance/photons/HIRun2018PbPb/L1T/photonTriggerAna_"$outputSuffix"_L1T_egBypassEGVetos1.root"
 );
 
 configList=(
 "Configurations/Performance/photons/HIRun2018PbPb/L1T/photonTriggerAna.Run2630XX.L1SingleEG.conf"
+"Configurations/Performance/photons/HIRun2018PbPb/L1T/photonTriggerAna.Run2630XX.L1T.conf"
+"Configurations/Performance/photons/HIRun2018PbPb/L1T/photonTriggerAna.Run2630XX.L1T.egBypassEGVetos1.conf"
 );
 
 arrayIndices=${!outList[*]}
