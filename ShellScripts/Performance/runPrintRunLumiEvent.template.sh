4020824c4e6f49137502982dd86e80d3f0066d10
Kaya Tatar <tatark@mit.edu>
2018-03-07 18:42:43 -0500

    example configuration and bash script to run printRunLumiEvent.C

---
+++ ShellScripts/Performance/runPrintRunLumiEvent.template.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+runCmd="./ShellScripts/myRun.sh"
+progPath="./Performance/printRunLumiEvent.exe"
+inputFileTmp="Configurations/filelists/Pythia8_AllQCDPhoton30Flt30_Hydjet_Cymbal_MB_HINPbPbWinter16DR_FOREST.list"
+inputFile="${inputFileTmp/.list/_TMP.list}"
+
+cat $inputFileTmp | head -10 > $inputFile
+
+configFiles=(
+"Configurations/examples/printRunLumiEvent.conf"
+);
+
+outDirBase=$EWJTAOUT
+if [ -z "$outDirBase" ]; then
+  outDirBase="/export/d00/scratch/"$USER"/EWJTA-out"
+fi
+outputSuffix="Pythia8_AllQCDPhoton30Flt30_Hydjet_Cymbal_MB"
+outputFiles=(
+$outDirBase"/Configurations/examples/printRLE_"$outputSuffix".txt"
+);
+
+arrayIndices=${!outputFiles[*]}
+for i1 in $arrayIndices
+do
+    configFile=${configFiles[i1]}
+    outputFile=${outputFiles[i1]}
+    outputFileLOG="${outputFile/.txt/.log}"
+    outDir=$(dirname "${outputFile}")
+    mkdir -p $outDir
+    $runCmd $progPath $configFile $inputFile $outputFile &> $outputFileLOG &
+    echo "$runCmd $progPath $configFile $inputFile $outputFile &> $outputFileLOG &"
+done
+
+
