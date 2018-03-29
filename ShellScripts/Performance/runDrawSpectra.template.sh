0eaf9958cc2bbab35a9c804d75548d73c9d538ef
Kaya Tatar <tatark@mit.edu>
2018-03-29 19:30:31 -0400

    change to more common array names

--- ShellScripts/Performance/runDrawSpectra.template.sh
+++ ShellScripts/Performance/runDrawSpectra.template.sh
@@ -5,12 +5,12 @@ progPath="./Performance/drawSpectra.exe"
 progCode="${progPath/.exe/.C}"
 #g++ $progCode $(root-config --cflags --libs) -Werror -Wall -Wno-narrowing -lTMVA -lRooFitCore -lRooFit -O2 -o $progPath || exit 1
 
-inputFiles=(
+inputList=(
 "/mnt/hadoop/cms/store/user/tatar/official/Pythia8_AllQCDPhoton30_Hydjet_Cymbal_MB/HINPbPbWinter16DR-75X_mcRun2_HeavyIon_v14-v1-FOREST/0.root"
 "/mnt/hadoop/cms/store/user/tatar/official/Pythia8_AllQCDPhoton30_Hydjet_Cymbal_MB/HINPbPbWinter16DR-75X_mcRun2_HeavyIon_v14-v1-FOREST/0.root"
 );
 
-configFiles=(
+configList=(
 "Configurations/photons/spectra.photon.conf"
 "Configurations/photons/spectra.phoEt.conf"
 );
@@ -20,17 +20,17 @@ if [ -z "$outDirBase" ]; then
   outDirBase="/export/d00/scratch/"$USER"/EWJTA-out"
 fi
 outputSuffix="Pythia8_AllQCDPhoton30_Hydjet_Cymbal_MB"
-outputFiles=(
+outList=(
 $outDirBase"/Configurations/photons/spectra_photon_"$outputSuffix".root"
 $outDirBase"/Configurations/photons/spectra_phoEt_"$outputSuffix".root"
 );
 
-arrayIndices=${!outputFiles[*]}
+arrayIndices=${!outList[*]}
 for i1 in $arrayIndices
 do
-    configFile=${configFiles[i1]}
-    inputFile=${inputFiles[i1]}
-    outputFile=${outputFiles[i1]}
+    configFile=${configList[i1]}
+    inputFile=${inputList[i1]}
+    outputFile=${outList[i1]}
     outputFileLOG="${outputFile/.root/.log}"
     outDir=$(dirname "${outputFile}")
     mkdir -p $outDir
