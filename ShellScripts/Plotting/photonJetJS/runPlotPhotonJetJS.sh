c684d2717b7e1d624ba57224d52f857af8e9bf3a
Kaya Tatar <tatark@mit.edu>
2018-04-06 15:32:26 -0400

    script to plot photon+jet JS results

---
+++ ShellScripts/Plotting/photonJetJS/runPlotPhotonJetJS.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -x
+
+g++ Plotting/photonJetFF/plotPhotonJetFF.C $(root-config --cflags --libs) -Werror -Wall -Wextra -Wno-narrowing -O2 -o Plotting/photonJetFF/plotPhotonJetFF.exe || exit 1
+
+progPath="./Plotting/photonJetFF/plotPhotonJetFF.exe"
+inputFiles=(
+"./Data/photonJetJS/jsdata_data_60_30_gxi0_obs2_ffjs_final-and-systematics.root"
+"./Data/photonJetJS/jsdata_data_60_30_gxi0_obs2_ffjs_final-and-systematics.root"
+);
+
+histLists=(
+"Configurations/photonJetJS/js.list"
+"Configurations/photonJetJS/js-cent030.list"
+);
+
+configList=(
+"Configurations/photonJetJS/js-60-30-gxi0.conf"
+"Configurations/photonJetJS/js-60-30-gxi0-cent030.conf"
+);
+
+arrayIndices=${!configList[*]}
+for i1 in $arrayIndices
+do
+    configFile=${configList[i1]}
+    outputFileLOG="${configFile/.conf/.log}"
+    inputFile=${inputFiles[i1]}
+    histList=${histLists[i1]}
+    $progPath $inputFile $histList $configFile &> $outputFileLOG
+done
+
