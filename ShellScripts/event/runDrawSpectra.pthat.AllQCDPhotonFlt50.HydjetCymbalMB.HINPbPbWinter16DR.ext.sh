3de5e5e2d6c5c4dd48461609e8a7e8bcc84d36cf
Kaya Tatar <tatark@mit.edu>
2018-04-10 12:17:25 -0400

    configurations and shell scripts for pthat spectra in extended Pythia8 AllQCDPhoton Flt50 Hydjet Cymbal MB samples

---
+++ ShellScripts/event/runDrawSpectra.pthat.AllQCDPhotonFlt50.HydjetCymbalMB.HINPbPbWinter16DR.ext.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#g++ -Wall -Werror -Wextra -O2 -Wno-narrowing `root-config --cflags --libs` -lTMVA -lRooFitCore -lRooFit -MMD -MF ./build/Performance/drawSpectra.d Performance/drawSpectra.C -o Performance/drawSpectra.exe
+
+runCmd="./ShellScripts/myRun.sh"
+progPath="./Performance/drawSpectra.exe"
+inputFiles=(
+"Configurations/filelists/Pythia8_AllQCDPhoton30Flt50_Hydjet_Cymbal_MB_HINPbPbWinter16DR_ext_FOREST.list"
+"Configurations/filelists/Pythia8_AllQCDPhoton50Flt50_Hydjet_Cymbal_MB_HINPbPbWinter16DR_ext_FOREST.list"
+"Configurations/filelists/Pythia8_AllQCDPhoton80Flt50_Hydjet_Cymbal_MB_HINPbPbWinter16DR_ext_FOREST.list"
+"Configurations/filelists/Pythia8_AllQCDPhoton120Flt50_Hydjet_Cymbal_MB_HINPbPbWinter16DR_ext_FOREST.list"
+"Configurations/filelists/Pythia8_AllQCDPhotonXXFlt50_Hydjet_Cymbal_MB_HINPbPbWinter16DR_ext_FOREST.list"
+);
+
+configFiles=(
+"Configurations/event/Pythia8_AllQCDPhotonFlt50_Hydjet_Cymbal_MB_HINPbPbWinter16DR_ext/spectra.pthat.conf"
+"Configurations/event/Pythia8_AllQCDPhotonFlt50_Hydjet_Cymbal_MB_HINPbPbWinter16DR_ext/spectra.pthat.conf"
+"Configurations/event/Pythia8_AllQCDPhotonFlt50_Hydjet_Cymbal_MB_HINPbPbWinter16DR_ext/spectra.pthat.conf"
+"Configurations/event/Pythia8_AllQCDPhotonFlt50_Hydjet_Cymbal_MB_HINPbPbWinter16DR_ext/spectra.pthat.conf"
+"Configurations/event/Pythia8_AllQCDPhotonFlt50_Hydjet_Cymbal_MB_HINPbPbWinter16DR_ext/spectra.pthat.conf"
+);
+
+outDirBase=$EWJTAOUT
+if [ -z "$outDirBase" ]; then
+  outDirBase="/export/d00/scratch/"$USER"/EWJTA-out"
+fi
+outList=(
+$outDirBase"/Configurations/event/spectra_pthat_AllQCDPhoton30Flt50_Hydjet_Cymbal_MB_HINPbPbWinter16DR_ext.root"
+$outDirBase"/Configurations/event/spectra_pthat_AllQCDPhoton50Flt50_Hydjet_Cymbal_MB_HINPbPbWinter16DR_ext.root"
+$outDirBase"/Configurations/event/spectra_pthat_AllQCDPhoton80Flt50_Hydjet_Cymbal_MB_HINPbPbWinter16DR_ext.root"
+$outDirBase"/Configurations/event/spectra_pthat_AllQCDPhoton120Flt50_Hydjet_Cymbal_MB_HINPbPbWinter16DR_ext.root"
+$outDirBase"/Configurations/event/spectra_pthat_AllQCDPhotonXXFlt50_Hydjet_Cymbal_MB_HINPbPbWinter16DR_ext.root"
+);
+
+arrayIndices=${!outList[*]}
+for i1 in $arrayIndices
+do
+    inputFile=${inputFiles[i1]}
+    configFile=${configFiles[i1]}
+    outputFile=${outList[i1]}
+    outputFileLOG="${outputFile/.root/.log}"
+    outDir=$(dirname "${outputFile}")
+    mkdir -p $outDir
+    $runCmd $progPath $configFile $inputFile $outputFile &> $outputFileLOG &
+    echo "$runCmd $progPath $configFile $inputFile $outputFile &> $outputFileLOG &"
+done
+
+
