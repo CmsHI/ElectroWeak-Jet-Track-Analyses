15f88198e04261e5b7c3fe33a9dcaddeef1a07f0
Kaya Tatar <tatark@mit.edu>
2018-05-07 18:37:59 -0400

    run JS figure that includes pp MC

--- ShellScripts/Plotting/photonJetJS/runPlotCompact.sh
+++ ShellScripts/Plotting/photonJetJS/runPlotCompact.sh
@@ -7,11 +7,18 @@ progPath="${progSrcPath/.C/.exe}"
 
 g++ $progSrcPath $(root-config --cflags --libs) -Werror -Wall -Wextra -Wno-narrowing -O2 -o $progPath
 
+fileData="./Data/photonJetJS/jsdata_data_60_30_gxi0_obs2_ffjs_final-and-systematics.root"
+filePPMC="./Data/photonJetJS/jsclosure_ppmc_60_30_gxi0_obs2_ffjs_final.root"
+fileDataMCRatio="./Data/photonJetJS/jsdata_pp_data_mc_ratio_60_30_gxi0_obs2.root"
+fileAll="./Data/photonJetJS/jsdata_data_60_30_gxi0_obs2_ffjs_final-and-systematics-PPMC-TMP.root"
+hadd -f $fileAll $fileData $filePPMC $fileDataMCRatio
+
 inputFiles=(
-"./Data/photonJetJS/jsdata_data_60_30_gxi0_obs2_ffjs_final-and-systematics.root"
+$fileData
+$fileAll
 );
 
-figureIndices=(1);
+figureIndices=(1 2);
 isJS=1
 
 arrayIndices=${!inputFiles[*]}
@@ -21,3 +28,5 @@ do
     figureIndex=${figureIndices[i1]}
     $progPath $inputFile $figureIndex $isJS
 done
+
+rm $fileAll
