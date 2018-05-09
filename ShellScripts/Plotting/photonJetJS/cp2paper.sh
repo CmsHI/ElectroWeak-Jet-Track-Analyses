09496a01cbdfe442d768a8fea13591d30b08b0ce
Kaya Tatar <tatark@mit.edu>
2018-05-09 19:28:33 -0400

    compact figure becomes the paper figure, current figures become supplementary

--- ShellScripts/Plotting/photonJetJS/cp2paper.sh
+++ ShellScripts/Plotting/photonJetJS/cp2paper.sh
@@ -2,7 +2,7 @@
 
 if [ $# -lt 1 ]; then
   echo "Usage: ./ShellScripts/Plotting/photonJetJS/cp2paper.sh [paper trunk dir]"
-  echo "Example: ./ShellScripts/Plotting/photonJetJS/cp2paper.sh /path/to/myDocuments/XXX-YY-ZZ/papers/XXX-YY-ZZ/trunk/"
+  echo "Example: ./ShellScripts/Plotting/photonJetJS/cp2paper.sh /path/to/myDocuments/XXX-YY-ZZZ/papers/XXX-YY-ZZZ/trunk/"
   exit 1
 fi
 
@@ -10,13 +10,15 @@ destinationDir=$1
 sourceDir="./"
 
 sourceFiles=(
+"photonjetJS_compact_fig2.pdf"
 "js_60_30_gxi0.pdf"
 "js_pp_data_mc.pdf"
 );
 
 destinationFiles=(
-"figures/jetshape_phoet60_jetpt30.pdf"
-"figures/jetshape_phoet60_jetpt30_pp_data_mc.pdf"
+"figures/jetshape_phoet60_jetpt30_paper.pdf"
+"figures/supplementary/jetshape_phoet60_jetpt30.pdf"
+"figures/supplementary/jetshape_phoet60_jetpt30_pp_data_mc.pdf"
 );
 
 arrayIndices=${!sourceFiles[*]}
