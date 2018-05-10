6acd1ce007a33b4b0fc4548d116ab29c0d283b7b
Kaya Tatar <tatark@mit.edu>
2018-05-10 13:24:40 -0400

    LBT photon+jet JS prediction data tables

--- Data/photonJetJS/photonJetJSData.h
+++ Data/photonJetJS/photonJetJSData.h
@@ -77,4 +77,91 @@ const double falphas[kN_DATATABLES] = {
 
 };
 
+namespace LBT {
+
+
+/*
+ * LBT photon+jet JS prediction data tables
+ * naming convention :
+ *      n_<observable>    : number of points in the observable
+ *      x_<observable>    : x axis values of the observable, bin centers
+ *      y_<observable>    : y axis values of the observable
+ */
+
+const std::string modelName = "LBT";
+
+const int n_rho = 6;
+const std::vector<double> x_r = {0.025, 0.075, 0.125, 0.175, 0.225, 0.275};
+
+// rho(r) distributions
+const std::vector<double> y_rho_pp = {12.9141, 3.70196, 1.61437, 0.92477, 0.565576, 0.279198};
+const std::vector<double> y_rho_pbpb_cent010 = {11.4205, 3.93139, 1.93818, 1.30264, 0.914969, 0.492328};
+
+// PbPb / pp ratio of rho(r) distributions
+const std::vector<double> y_rho_ratio_pbpb_pp_cent010 = {0.884341, 1.06198, 1.20058, 1.40861, 1.61776, 1.76337};
+
+enum DATATABLES{
+    k_rho_pp,
+    k_rho_pbpb_cent010,
+    k_rho_ratio_pbpb_pp_cent010,
+    kN_DATATABLES
+};
+
+const std::string dataNames[kN_DATATABLES] = {
+        "rho_pp",
+        "rho_pbpb_cent010",
+        "rho_ratio_pbpb_pp_cent010"
+};
+
+const std::vector<double> xData[kN_DATATABLES] = {
+        x_r,
+        x_r,
+        x_r
+};
+
+const std::vector<double> yData[kN_DATATABLES] = {
+        y_rho_pp,
+        y_rho_pbpb_cent010,
+        y_rho_ratio_pbpb_pp_cent010
+};
+
+const std::string xTitles[kN_DATATABLES] = {
+        "r",
+        "r",
+        "r"
+};
+
+const std::string yTitles[kN_DATATABLES] = {
+        "\rho(r)_{pp}",
+        "\rho(r)_{PbPb}",
+        "\rho(r)_{PbPb} / \rho(r)_{pp}"
+};
+
+const std::string titles[kN_DATATABLES] = {
+        "LBT",
+        "LBT",
+        "LBT"
+};
+
+// fill colors to be used in a plot
+const int colors[kN_DATATABLES] = {
+        kBlue,
+        kBlue,
+        kBlue
+};
+
+const int lineStyles[kN_DATATABLES] = {
+        kDashed,
+        kDashed,
+        kBlue
+};
+
+const double falphas[kN_DATATABLES] = {
+        0.7,
+        0.7,
+        0.7
+};
+
+};
+
 #endif /* DATA_PHOTONJETJS_PHOTONJETJSDATA_H_ */
