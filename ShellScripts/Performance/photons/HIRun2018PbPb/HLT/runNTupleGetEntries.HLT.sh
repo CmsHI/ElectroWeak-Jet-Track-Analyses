bc5f83da7c144585d0a9e4343873f4c0d3bd9862
Kaya Tatar <tatark@mit.edu>
2018-10-23 00:04:03 +0200

    get counts for Cent30_100 and Cent50_100 gated paths. Photon15 is removed from menu.

--- ShellScripts/Performance/photons/HIRun2018PbPb/HLT/runNTupleGetEntries.HLT.sh
+++ ShellScripts/Performance/photons/HIRun2018PbPb/HLT/runNTupleGetEntries.HLT.sh
@@ -9,11 +9,12 @@ inputList=(
 ## hltTestEgamma
 #"/mnt/hadoop/cms/store/user/katatar/HIRun2018PbPb/HLT/Hydjet_Quenched_Drum5Ev8_PbPbMinBias_5020GeV_2018/hltTestEgamma_V32/openHLT_merged.root"
 #"/mnt/hadoop/cms/store/user/katatar/HIRun2018PbPb/HLT/HIMinimumBias8_XeXeRun2017/hltTestEgamma_V32/openHLT_merged.root"
+"/mnt/hadoop/cms/store/user/katatar/HIRun2018PbPb/HLT/Hydjet_Quenched_Drum5Ev8_PbPbMinBias_5020GeV_2018/hltTestEgamma_V68/openHLT_merged.root"
 "/mnt/hadoop/cms/store/user/katatar/HIRun2018PbPb/HLT/Pythia8_AllQCDPhoton15_Hydjet_Quenched_Cymbal5Ev8/hltTestEgamma_V68/openHLT_merged.root"
 ## hltPbPb2018Photons
 #"/mnt/hadoop/cms/store/user/katatar/HIRun2018PbPb/HLT/Hydjet_Quenched_Drum5Ev8_PbPbMinBias_5020GeV_2018/hltPbPb2018Photons_V10/openHLT_merged.root"
 #"/mnt/hadoop/cms/store/user/katatar/HIRun2018PbPb/HLT/Hydjet_Quenched_Drum5Ev8_PbPbMinBias_5020GeV_2018/hltPbPb2018Photons_V11/openHLT_merged.root"
-"/mnt/hadoop/cms/store/user/katatar/HIRun2018PbPb/HLT/Pythia8_AllQCDPhoton15_Hydjet_Quenched_Cymbal5Ev8/hltPbPb2018Photons_V18/openHLT_merged.root"
+#"/mnt/hadoop/cms/store/user/katatar/HIRun2018PbPb/HLT/Pythia8_AllQCDPhoton15_Hydjet_Quenched_Cymbal5Ev8/hltPbPb2018Photons_V18/openHLT_merged.root"
 );
 
 outDirBase=$EWJTAOUT
@@ -26,58 +27,71 @@ outList=(
 ## hltTestEgamma
 #$outDirBase"/Performance/photons/HIRun2018PbPb/HLT/nTupleGetEntries_"$suffixHydjet"_hltTestEgamma_V32.log"
 #$outDirBase"/Performance/photons/HIRun2018PbPb/HLT/nTupleGetEntries_HIMinimumBias8_XeXeRun2017_hltTestEgamma_V32.log"
+$outDirBase"/Performance/photons/HIRun2018PbPb/HLT/nTupleGetEntries_"$suffixHydjet"_hltTestEgamma_V68.log"
 $outDirBase"/Performance/photons/HIRun2018PbPb/HLT/nTupleGetEntries_"$suffixAllQCDPhoton"_hltTestEgamma_V68.log"
 ## hltPbPb2018Photons
 #$outDirBase"/Performance/photons/HIRun2018PbPb/HLT/nTupleGetEntries_"$suffixHydjet"_hltPbPb2018Photons_V10.log"
 #$outDirBase"/Performance/photons/HIRun2018PbPb/HLT/nTupleGetEntries_"$suffixHydjet"_hltPbPb2018Photons_V11.log"
-$outDirBase"/Performance/photons/HIRun2018PbPb/HLT/nTupleGetEntries_"$suffixAllQCDPhoton"_hltPbPb2018Photons_V18.log"
+#$outDirBase"/Performance/photons/HIRun2018PbPb/HLT/nTupleGetEntries_"$suffixAllQCDPhoton"_hltPbPb2018Photons_V18.log"
 );
 
 argFileTmp="./Performance/nTupleGetEntries.args.txt"
 rm -f $argFileTmp
 echo "==selections==" >> $argFileTmp
 echo "HLT_HIGEDPhoton10_v1 > 0" >> $argFileTmp
-echo "HLT_HIGEDPhoton15_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton20_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton30_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton40_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton50_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton60_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton10_HECut_v1 > 0" >> $argFileTmp
-echo "HLT_HIGEDPhoton15_HECut_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton20_HECut_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton30_HECut_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton40_HECut_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton50_HECut_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton60_HECut_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton10_EB_v1 > 0" >> $argFileTmp
-echo "HLT_HIGEDPhoton15_EB_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton20_EB_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton30_EB_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton40_EB_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton50_EB_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton60_EB_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton10_EB_HECut_v1 > 0" >> $argFileTmp
-echo "HLT_HIGEDPhoton15_EB_HECut_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton20_EB_HECut_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton30_EB_HECut_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton40_EB_HECut_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton50_EB_HECut_v1 > 0" >> $argFileTmp
 echo "HLT_HIGEDPhoton60_EB_HECut_v1 > 0" >> $argFileTmp
 echo "HLT_HIIslandPhoton10_Eta3p1_v1 > 0" >> $argFileTmp
-echo "HLT_HIIslandPhoton15_Eta3p1_v1 > 0" >> $argFileTmp
 echo "HLT_HIIslandPhoton20_Eta3p1_v1 > 0" >> $argFileTmp
 echo "HLT_HIIslandPhoton30_Eta3p1_v1 > 0" >> $argFileTmp
 echo "HLT_HIIslandPhoton40_Eta3p1_v1 > 0" >> $argFileTmp
 echo "HLT_HIIslandPhoton50_Eta3p1_v1 > 0" >> $argFileTmp
 echo "HLT_HIIslandPhoton60_Eta3p1_v1 > 0" >> $argFileTmp
 echo "HLT_HIIslandPhoton10_Eta1p5_v1 > 0" >> $argFileTmp
-echo "HLT_HIIslandPhoton15_Eta1p5_v1 > 0" >> $argFileTmp
 echo "HLT_HIIslandPhoton20_Eta1p5_v1 > 0" >> $argFileTmp
 echo "HLT_HIIslandPhoton30_Eta1p5_v1 > 0" >> $argFileTmp
 echo "HLT_HIIslandPhoton40_Eta1p5_v1 > 0" >> $argFileTmp
 echo "HLT_HIIslandPhoton50_Eta1p5_v1 > 0" >> $argFileTmp
 echo "HLT_HIIslandPhoton60_Eta1p5_v1 > 0" >> $argFileTmp
+## Cent30_100
+echo "HLT_HIGEDPhoton10_Cent30_100_v1 > 0" >> $argFileTmp
+echo "HLT_HIGEDPhoton20_Cent30_100_v1 > 0" >> $argFileTmp
+echo "HLT_HIGEDPhoton30_Cent30_100_v1 > 0" >> $argFileTmp
+echo "HLT_HIGEDPhoton40_Cent30_100_v1 > 0" >> $argFileTmp
+echo "HLT_HIIslandPhoton10_Eta3p1_Cent30_100_v1 > 0" >> $argFileTmp
+echo "HLT_HIIslandPhoton20_Eta3p1_Cent30_100_v1 > 0" >> $argFileTmp
+echo "HLT_HIIslandPhoton30_Eta3p1_Cent30_100_v1 > 0" >> $argFileTmp
+echo "HLT_HIIslandPhoton40_Eta3p1_Cent30_100_v1 > 0" >> $argFileTmp
+## Cent50_100
+echo "HLT_HIGEDPhoton10_Cent50_100_v1 > 0" >> $argFileTmp
+echo "HLT_HIGEDPhoton20_Cent50_100_v1 > 0" >> $argFileTmp
+echo "HLT_HIGEDPhoton30_Cent50_100_v1 > 0" >> $argFileTmp
+echo "HLT_HIGEDPhoton40_Cent50_100_v1 > 0" >> $argFileTmp
+echo "HLT_HIIslandPhoton10_Eta3p1_Cent50_100_v1 > 0" >> $argFileTmp
+echo "HLT_HIIslandPhoton20_Eta3p1_Cent50_100_v1 > 0" >> $argFileTmp
+echo "HLT_HIIslandPhoton30_Eta3p1_Cent50_100_v1 > 0" >> $argFileTmp
+echo "HLT_HIIslandPhoton40_Eta3p1_Cent50_100_v1 > 0" >> $argFileTmp
 echo "==trees==" >> $argFileTmp
 echo "hltbitanalysis/HltTree" >> $argFileTmp
 
@@ -86,10 +100,11 @@ argFiles=(
 #$argFileTmp
 #$argFileTmp
 $argFileTmp
+$argFileTmp
 ## hltPbPb2018Photons
 #$argFileTmp
 #$argFileTmp
-$argFileTmp
+#$argFileTmp
 );
 
 arrayIndices=${!outList[*]}
