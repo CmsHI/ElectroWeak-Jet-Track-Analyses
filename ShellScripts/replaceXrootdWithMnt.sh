e07c7172d4a6f816cb8d105d13970082e61cb7e7
Kaya Tatar <tatark@mit.edu>
2018-11-27 18:42:07 +0100

    simple script to replace the "xroot" path with "/mnt"

---
+++ ShellScripts/replaceXrootdWithMnt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# simple script to replace the "xroot" path with "/mnt".
+
+if [ $# -lt 1 ]; then
+  echo "Usage: ./ShellScripts/replaceXrootdWithMnt.sh [inputFile]"
+  echo "Example: ./ShellScripts/replaceXrootdWithMnt.sh Configurations/filelists/Pythia8_AllQCDPhoton30Flt30_Hydjet_Cymbal_MB_HINPbPbWinter16DR_FOREST.list"
+  exit 1
+fi
+
+inputFile=$1
+if [[ $inputFile = *.root ]];  # if input is a ROOT file, then just print the new file path
+then
+  echo $inputFile | sed "s,root://xrootd.cmsaf.mit.edu//store,/mnt/hadoop/cms/store,g"
+else
+  sed -i "s,root://xrootd.cmsaf.mit.edu//store,/mnt/hadoop/cms/store,g" $inputFile
+fi
+
+
