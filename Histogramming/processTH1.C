73bc51bf63a083261fd271f87a607555fd2bb0a5
Kaya Tatar <tatark@mit.edu>
2019-02-08 11:51:50 -0500

    define operations for processTH1. Operations (as of now) are add, subtract, multiply, divide, and unit normalization.

--- Histogramming/processTH1.C
+++ Histogramming/processTH1.C
@@ -17,50 +17,119 @@
 #include "../Utilities/systemUtil.h"
 
 void setTH1D(TH1D* h);
-void processTH1(std::string inputFile, std::string outputFile = "processTH1.root", std::string writeMode = "RECREATE", std::string hInPath = "", std::string hOutPath = "");
+void processTH1(std::string inputFiles, std::string outputFile = "processTH1.root", std::string writeMode = "RECREATE", std::string hInPaths = "", std::string hOutPath = "", std::string operation = "add");
 
-void processTH1(std::string inputFile, std::string outputFile, std::string writeMode, std::string hInPath, std::string hOutPath)
+void processTH1(std::string inputFiles, std::string outputFile, std::string writeMode, std::string hInPaths, std::string hOutPath, std::string operation)
 {
-    std::cout<<"running processTH1()"<<std::endl;
-    std::cout<<"inputFile  = "<< inputFile.c_str()  <<std::endl;
-    std::cout<<"outputFile = "<< outputFile.c_str() <<std::endl;
-    std::cout<<"writeMode  = "<< writeMode.c_str() <<std::endl;
-    std::cout<<"hInPath  = "<< hInPath.c_str() <<std::endl;
-    std::cout<<"hOutPath = "<< hOutPath.c_str() <<std::endl;
+    std::cout << "running processTH1()" << std::endl;
+    std::cout << "inputFiles = " << inputFiles.c_str()  << std::endl;
+    std::cout << "outputFile = " << outputFile.c_str() << std::endl;
+    std::cout << "writeMode  = " << writeMode.c_str() << std::endl;
+    std::cout << "hInPaths  = " << hInPaths.c_str() << std::endl;
+    std::cout << "hOutPath = " << hOutPath.c_str() << std::endl;
+    std::cout << "operation = " << operation.c_str() << std::endl;
+
+    operation = toLowerCase(operation);
 
     // TH1 objects
     TH1::SetDefaultSumw2();
 
-    TFile* input = 0;
-    input = TFile::Open(inputFile.c_str(), "READ");
-    input->cd();
+    std::vector<std::string> inputFilePaths = split(inputFiles, ",", false, false);
+    int nInputFiles = inputFilePaths.size();
+
+    std::vector<std::string> hInputPaths = split(hInPaths, ",", false, false);
+    int nInputHist = hInputPaths.size();
+
+    if (nInputFiles != 1 && nInputHist != 1 && nInputFiles != nInputHist) {
+        std::cout << "Mismatch in number of input files and number of input histograms." << std::endl;
+        std::cout << "nInputFiles = " << nInputFiles << std::endl;
+        std::cout << "nInputHist = " << nInputHist << std::endl;
+        std::cout << "Exiting." << std::endl;
+        return;
+    }
+    if (operation == "unitnorm" && nInputHist > 1) {
+        std::cout << "There should be only one input histogram if operation is unit normalization." << std::endl;
+        std::cout << "nInputHist = " << nInputHist << std::endl;
+        std::cout << "Exiting." << std::endl;
+        return;
+    }
+
+    std::cout << "nInputFiles = " << nInputFiles << std::endl;
+    for (int i = 0; i < nInputFiles; ++i) {
+        std::cout << Form("inputFiles[%d] = %s", i, inputFilePaths.at(i).c_str()) << std::endl;
+    }
+
+    std::cout << "nInputHist = " << nInputHist << std::endl;
+    for (int i = 0; i < nInputHist; ++i) {
+        std::cout << Form("hInPaths[%d] = %s", i, hInputPaths.at(i).c_str()) << std::endl;
+    }
+
+    std::vector<TFile*> inputs(nInputFiles, 0);
+    for (int i = 0; i < nInputFiles; ++i) {
+        inputs[i] = 0;
+        inputs[i] = TFile::Open(inputFilePaths.at(i).c_str(), "READ");
+    }
+
+    std::vector<TH1D*> hInVec(nInputHist, 0);
+    for (int i = 0; i < nInputHist; ++i) {
+
+        hInVec[i] = 0;
+        int iFile = (nInputFiles == 1) ? 0 : i;
+        hInVec[i] = (TH1D*)inputs[iFile]->Get(hInputPaths[i].c_str());
+        if (hInVec[i] == 0) continue;
+    }
 
     TFile* output = TFile::Open(outputFile.c_str(), writeMode.c_str());
     output->cd();
 
-    TH1D* hIn = 0;
-    //TH2D* h2DIn = 0;
-
     TH1D* hOut = 0;
-    //TH2D* h2DOut = 0;
+    if (operation == "add") {
+        hOut = (TH1D*)hInVec[0]->Clone(hOutPath.c_str());
+        for (int i = 1; i < nInputHist; ++i) {
+            hOut->Add(hInVec[i]);
+        }
+    }
+    else if (operation == "sub") {
+        hOut = (TH1D*)hInVec[0]->Clone(hOutPath.c_str());
+        for (int i = 1; i < nInputHist; ++i) {
+            hOut->Add(hInVec[i], -1);
+        }
+    }
+    else if (operation == "mul") {
+        hOut = (TH1D*)hInVec[0]->Clone(hOutPath.c_str());
+        for (int i = 1; i < nInputHist; ++i) {
+            hOut->Multiply(hInVec[i]);
+        }
+    }
+    else if (operation == "div") {
+        hOut = (TH1D*)hInVec[0]->Clone(hOutPath.c_str());
+        for (int i = 1; i < nInputHist; ++i) {
+            hOut->Divide(hInVec[i]);
+        }
+    }
+    else if (operation == "unitnorm") {
+        hOut = (TH1D*)hInVec[0]->Clone(hOutPath.c_str());
+        hOut->Scale(1.0 / hOut->Integral(), "width");
+    }
+    else {
+        hOut = (TH1D*)hInVec[0]->Clone(hOutPath.c_str());
+    }
 
-    hIn = 0;
-    hIn = (TH1D*)input->Get(hInPath.c_str());
-    if (hIn == 0) return;
-
-    hOut = (TH1D*)hIn->Clone(hOutPath.c_str());
     setTH1D(hOut);
-    hOut->Scale(1.0 / hOut->Integral(), "width");
     hOut->Write("",TObject::kOverwrite);
 
-    std::cout<<"Closing the output file."<<std::endl;
+    std::cout << "Closing the output file." << std::endl;
     output->Close();
-    std::cout<<"running processTH1() - END"<<std::endl;
+    std::cout << "running processTH1() - END" << std::endl;
 }
 
 int main(int argc, char** argv)
 {
-    if (argc == 6) {
+    if (argc == 7) {
+        processTH1(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6]);
+        return 0;
+    }
+    else if (argc == 6) {
         processTH1(argv[1], argv[2], argv[3], argv[4], argv[5]);
         return 0;
     }
@@ -78,7 +147,7 @@ int main(int argc, char** argv)
     }
     else {
         std::cout << "Usage : \n" <<
-                "./Histogramming/processTH1.exe <inputFile> <outputFile> <writeMode>"
+                "./Histogramming/processTH1.exe <inputFiles> <outputFile> <writeMode> <hInPaths> <hOutPath> <operation>"
                 << std::endl;
         return 1;
     }
--- ShellScripts/Histogramming/runProcessTH1.sh
+++ ShellScripts/Histogramming/runProcessTH1.sh
@@ -6,39 +6,77 @@ progPath="./Histogramming/processTH1.exe"
 progCode="${progPath/.exe/.C}"
 g++ $progCode $(root-config --cflags --libs) -Werror -Wall -Wextra -Wno-narrowing -lTMVA -lRooFitCore -lRooFit -O2 -o $progPath || exit 1
 
-inputList=(
-"./Data/zBosons/zJet_ResultData.root"
-"./Data/zBosons/zJet_ResultData.root"
+input1="./Data/zBosons/zJet_ResultData.root"
+inputsList=(
+$input1
+$input1
+$input1
+$input1","$input1
+$input1","$input1
+$input1","$input1
+$input1","$input1
+$input1","$input1
 );
 
 outList=(
 "./Histogramming/processTH1.root"
 "./Histogramming/processTH1.root"
+"./Histogramming/processTH1.root"
+"./Histogramming/processTH1.root"
+"./Histogramming/processTH1.root"
+"./Histogramming/processTH1.root"
+"./Histogramming/processTH1.root"
+"./Histogramming/processTH1.root"
 );
 
-hInPathList=(
+hInPathsList=(
+"h1D_xjz_pp"
 "h1D_xjz_pp"
 "h1D_xjz_pbpb_cent030"
+"h1D_xjz_pbpb_cent030,h1D_xjz_pp"
+"h1D_rjz_pbpb_cent030,h1D_rjz_pp"
+"h1D_xjzMean_pp,h1D_rjz_pp"
+"h1D_xjz_pp_cent030,h1D_xjz_pp"
+"h1D_xjzMean_pbpb_cent030,h1D_xjzMean_pp"
 );
 
 hOutPathList=(
+"h1D_xjz_pp"
 "h1D_xjz_pp_unitNorm"
 "h1D_xjz_pbpb_cent030_unitNorm"
+"h1D_xjz_pbpb_cent030_pp_SUMMED"
+"h1D_rjz_pbpb_cent030_pp_SUBTRACTED"
+"h1D_xjzMean_rjz_pp_MULTIPLIED"
+"h1D_xjz_pp_cent030_pp_DIVIDED"
+"h1D_xjzMean_pbpb_cent030_pp_DIVIDED"
 );
 
 writeMode="UPDATE"
 
+operationList=(
+"NONE"
+"unitnorm"
+"unitnorm"
+"add"
+"sub"
+"mul"
+"div"
+"div"
+);
+
+
 arrayIndices=${!outList[*]}
 for i1 in $arrayIndices
 do
-    inputFile=${inputList[i1]}
+    inputFiles=${inputsList[i1]}
     outputFile=${outList[i1]}
-    hInPath=${hInPathList[i1]}
+    hInPaths=${hInPathsList[i1]}
     hOutPath=${hOutPathList[i1]}
-    outputFileLOG="${outputFile/.root/.log}"
+    operation=${operationList[i1]}
+    outputFileLOG="${outputFile/.root/_${hOutPath}.log}"
     outDir=$(dirname "${outputFile}")
     mkdir -p $outDir
-    $runCmd $progPath $inputFile $outputFile $writeMode $hInPath $hOutPath &> $outputFileLOG
-    echo "$runCmd $progPath $inputFile $outputFile $writeMode $hInPath $hOutPath &> $outputFileLOG"
+    $runCmd $progPath $inputFiles $outputFile $writeMode $hInPaths $hOutPath $operation &> $outputFileLOG
+    echo "$runCmd $progPath $inputFiles $outputFile $writeMode $hInPaths $hOutPath $operation &> $outputFileLOG"
 done
 
