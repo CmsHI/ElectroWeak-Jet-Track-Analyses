f6c3c47e9b30de6200f868b370a99562e6d63b31
Kaya Tatar <tatark@mit.edu>
2019-07-17 01:19:11 -0400

    add eventSkim class, reads/writes additional event info that is not in hiEvtAnalyzer

---
+++ TreeHeaders/eventSkimTree.h
@@ -0,0 +1,53 @@
+/*
+ * reads/writes additional event info that is not in hiEvtAnalyzer
+ */
+#ifndef TREEHEADERS_EVENTSKIMTREE_H_
+#define TREEHEADERS_EVENTSKIMTREE_H_
+
+#include <TTree.h>
+#include <TMath.h>
+#include <TVector3.h>
+
+#include <vector>
+
+class eventSkim {
+public :
+    eventSkim(){
+
+    }
+    ~eventSkim(){};
+    void setupTreeForReading(TTree *t);
+    void setupTreeForWriting(TTree *t);
+    void clearEvent();
+
+    // objects from mixed event
+    float pf_h_HF_totE;
+    float pf_eg_HF_totE;
+
+     // List of branches
+    TBranch *b_pf_h_HF_totE;
+    TBranch *b_pf_eg_HF_totE;
+};
+
+void eventSkim::setupTreeForReading(TTree *t)
+{
+    b_pf_h_HF_totE = 0;
+    b_pf_eg_HF_totE = 0;
+
+    if (t->GetBranch("pf_h_HF_totE"))  t->SetBranchAddress("pf_h_HF_totE", &pf_h_HF_totE, &b_pf_h_HF_totE);
+    if (t->GetBranch("pf_eg_HF_totE"))  t->SetBranchAddress("pf_eg_HF_totE", &pf_eg_HF_totE, &b_pf_eg_HF_totE);
+}
+
+void eventSkim::setupTreeForWriting(TTree *t)
+{
+    t->Branch("pf_h_HF_totE", &pf_h_HF_totE);
+    t->Branch("pf_eg_HF_totE", &pf_eg_HF_totE);
+}
+
+void eventSkim::clearEvent()
+{
+    pf_h_HF_totE = 0;
+    pf_eg_HF_totE = 0;
+}
+
+#endif /* TREEHEADERS_EVENTSKIMTREE_H_ */
