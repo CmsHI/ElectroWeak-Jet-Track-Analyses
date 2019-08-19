e3ad09307f2a1b9e822494dfbeeef8267047d5b6
Kaya Tatar <tatark@mit.edu>
2019-08-18 23:23:56 -0400

    trk ID for pp 2017

--- Utilities/vJetTrkUtil.h
+++ Utilities/vJetTrkUtil.h
@@ -512,8 +512,15 @@ bool passedTrkSelection(trackSkim& trks, int i, int collType)
         float Et = ((*trks.p_pfHcal)[i] + (*trks.p_pfEcal)[i]) / TMath::CosH((*trks.p_trkEta)[i]);
         if ((*trks.p_trkPt)[i] > 20 && !(Et > 0.5 * (*trks.p_trkPt)[i]))  return false;
     }
+    else if (collisionIsPP2017((COLL::TYPE)collType)) {
+        // cuts for pp 2017
+        if (!((*trks.p_highPurity)[i] == 1)) return false;
+        if (!((*trks.p_trkPtError)[i] / (*trks.p_trkPt)[i] < 0.1 &&
+              std::fabs((*trks.p_trkDz1)[i] / (*trks.p_trkDzError1)[i]) < 3 &&
+              std::fabs((*trks.p_trkDxy1)[i] / (*trks.p_trkDxyError1)[i]) < 3))  return false;
+    }
     else if (collisionIsPP((COLL::TYPE)collType)) {
-        // cuts for pp
+        // cuts for pp 2015
         if (!((*trks.p_trkPt)[i] <= 300 && std::fabs((*trks.p_trkEta)[i]) < 2.4)) return false;
         if (!((*trks.p_highPurity)[i] == 1)) return false;
         if (!((*trks.p_trkPtError)[i] / (*trks.p_trkPt)[i] < 0.1 &&
@@ -556,8 +563,15 @@ bool passedTrkSelection(Tracks& trks, int i, int collType)
         float Et = (trks.pfHcal[i] + trks.pfEcal[i]) / TMath::CosH(trks.trkEta[i]);
         if (trks.trkPt[i] > 20 && !(Et > 0.5 * trks.trkPt[i]))  return false;
     }
+    else if (collisionIsPP2017((COLL::TYPE)collType)) {
+        // cuts for pp 2017
+        if (!(trks.highPurity[i] == 1)) return false;
+        if (!(trks.trkPtError[i] / trks.trkPt[i] < 0.1 &&
+              std::fabs(trks.trkDz1[i] / trks.trkDzError1[i]) < 3 &&
+              std::fabs(trks.trkDxy1[i] / trks.trkDxyError1[i]) < 3))  return false;
+    }
     else if (collisionIsPP((COLL::TYPE)collType)) {
-        // cuts for pp
+        // cuts for pp 2015
         if (!(trks.trkPt[i] <= 300 && std::fabs(trks.trkEta[i]) < 2.4)) return false;
         if (!(trks.highPurity[i] == 1)) return false;
         if (!(trks.trkPtError[i] / trks.trkPt[i] < 0.1 &&
