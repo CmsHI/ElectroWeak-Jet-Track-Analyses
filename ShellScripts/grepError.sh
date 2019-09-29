9b5fecb54e8576520b4c517bebc236e77b18a58f
Kaya Tatar <tatark@mit.edu>
2019-09-28 22:14:06 -0400

    do not check binary files

--- ShellScripts/grepError.sh
+++ ShellScripts/grepError.sh
@@ -11,8 +11,12 @@ fi
 files=( $@ )
 for file in "${files[@]}"
 do
-  #echo ${file}
-  #grep -i --color "Segmentation" ${file}
-  #grep -i --color "Killed" ${file}
-  grep --color -e "Killed" -e "Aborted" -e "Bus error" -e "egmentation" -e "gfal-copy error" -e "error writing" -e "srm client error" -e "Unable to open" ${file}
+  if [[ ${file} == *.exe || ${file} == *.root ]]; then  # do not check binary files
+    echo "File "${file}" is binary. Skipping file".
+  else
+    #echo ${file}
+    #grep -i --color "Segmentation" ${file}
+    #grep -i --color "Killed" ${file}
+    grep --color -e "Killed" -e "Aborted" -e "Bus error" -e "egmentation" -e "gfal-copy error" -e "error writing" -e "srm client error" -e "Unable to open" ${file}
+  fi
 done
