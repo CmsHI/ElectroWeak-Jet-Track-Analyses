be3619499cb2afec0a0808a95f5d10750fd9dd27
Kaya Tatar <tatark@mit.edu>
2020-01-23 16:17:27 -0500

    test case

--- ForestSkimmers/vJetTrk/vJetTrkSkim.C
+++ ForestSkimmers/vJetTrk/vJetTrkSkim.C
@@ -1178,6 +1178,7 @@ void vJetTrkSkim(std::string configFile, std::string inputFile, std::string outp
                     //iCent = getPFEnergyBin((evtskim.pf_h_HF_totE + evtskim.pf_eg_HF_totE) - 699.0, totEMax); // zee
                     //iCent = getPFEnergyBin((evtskim.pf_h_HF_totE + evtskim.pf_eg_HF_totE) - 700, totEMax);  // mc_mg5
                     //iCent = getPFEnergyBin((evtskim.pf_h_HF_totE + evtskim.pf_eg_HF_totE) - 982.0, totEMax);
+                    //iCent = getPFEnergyBin((evtskim.pf_h_HF_totE + evtskim.pf_eg_HF_totE) - 0, totEMax);
                     // data
                     //iCent = getPFEnergyBin((evtskim.pf_h_HF_totE + evtskim.pf_eg_HF_totE) - 660, totEMax);
 
