eed10e738b10b101556fb1ef827bf62dd660ce86
Kaya Tatar <kaya.tatar@cern.ch>
2021-11-15 17:05:41 +0100

    PF vtx info, following with https://github.com/CmsHI/cmssw/pull/336

--- TreeHeaders/pfCandTree.h
+++ TreeHeaders/pfCandTree.h
@@ -17,6 +17,9 @@ public :
         pfEta = 0;
         pfPhi = 0;
         pfM = 0;
+        pfvx = 0;
+        pfvy = 0;
+        pfvz = 0;
         pfEcalE = 0;
         pfEcalEraw = 0;
         pfHcalE = 0;
@@ -40,6 +43,9 @@ public :
     std::vector<float>   *pfEta;
     std::vector<float>   *pfPhi;
     std::vector<float>   *pfM;
+    std::vector<float>   *pfvx;
+    std::vector<float>   *pfvy;
+    std::vector<float>   *pfvz;
     std::vector<float>   *pfEcalE;
     std::vector<float>   *pfEcalEraw;
     std::vector<float>   *pfHcalE;
@@ -59,6 +65,9 @@ public :
     TBranch        *b_pfEta;   //!
     TBranch        *b_pfPhi;   //!
     TBranch        *b_pfM;   //!
+    TBranch        *b_pfvx;   //!
+    TBranch        *b_pfvy;   //!
+    TBranch        *b_pfvz;   //!
     TBranch        *b_pfEcalE;   //!
     TBranch        *b_pfEcalEraw;   //!
     TBranch        *b_pfHcalE;   //!
@@ -80,6 +89,9 @@ void pfCand::setupTreeForReading(TTree *t)
     b_pfEta = 0;
     b_pfPhi = 0;
     b_pfM = 0;
+    b_pfvx = 0;
+    b_pfvy = 0;
+    b_pfvz = 0;
     b_pfEcalE = 0;
     b_pfEcalEraw = 0;
     b_pfHcalE = 0;
@@ -99,6 +111,9 @@ void pfCand::setupTreeForReading(TTree *t)
     if (t->GetBranch("pfEta"))  t->SetBranchAddress("pfEta", &pfEta, &b_pfEta);
     if (t->GetBranch("pfPhi"))  t->SetBranchAddress("pfPhi", &pfPhi, &b_pfPhi);
     if (t->GetBranch("pfM"))  t->SetBranchAddress("pfM", &pfM, &b_pfM);
+    if (t->GetBranch("pfvx"))  t->SetBranchAddress("pfvx", &pfvx, &b_pfvx);
+    if (t->GetBranch("pfvy"))  t->SetBranchAddress("pfvy", &pfvy, &b_pfvy);
+    if (t->GetBranch("pfvz"))  t->SetBranchAddress("pfvz", &pfvz, &b_pfvz);
     if (t->GetBranch("pfEcalE"))  t->SetBranchAddress("pfEcalE", &pfEcalE, &b_pfEcalE);
     if (t->GetBranch("pfEcalEraw"))  t->SetBranchAddress("pfEcalEraw", &pfEcalEraw, &b_pfEcalEraw);
     if (t->GetBranch("pfHcalE"))  t->SetBranchAddress("pfHcalE", &pfHcalE, &b_pfHcalE);
